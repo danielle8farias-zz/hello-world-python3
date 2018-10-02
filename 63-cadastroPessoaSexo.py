10ddaf80105c5b2f844cee5ff3d573d0699d9805
Danielle Farias <danielle8farias@gmail.com>
2018-10-02 16:05:00 -0300

    exercício com while e break

--- 63-cadastroPessoaSexo.py
+++ 63-cadastroPessoaSexo.py
@@ -13,13 +13,17 @@ while True:
     idade = int(input('Digite a idade: '))
     if idade > 18:
         maior_de_dezoito += 1
-    genero = input('Digite o sexo: [F/M] ').upper().strip()[0]
+    genero = ' '
+    while genero not in 'FM':
+        genero = input('Digite o sexo: [F/M] ').upper().strip()[0]
     if genero in 'M':
         quantidade_homem += 1
     elif genero in 'F' and idade < 20:
         mulheres_menores += 1
-    escolha = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if escolha != 'S':
+    escolha = ' '
+    while escolha not in 'SN':
+        escolha = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if escolha == 'N':
         break
 print('Total de pessoas com mais de 18 anos: {}'.format(maior_de_dezoito))
 print('Ao todo temos {} homens cadastrados.'.format(quantidade_homem))

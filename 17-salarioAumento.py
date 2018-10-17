828963771832f461ba6ef6f59af56ce47cfba3a2
Danielle Farias <danielle8farias@gmail.com>
2018-10-16 21:39:42 -0300

    atualizações de string

--- 17-salarioAumento.py
+++
@@ -1,9 +0,0 @@
-'''
-Leia o salário e o aumente em 15%.
-'''
-print('-'*50)
-print('{: ^50}'.format('SALÁRIO AUMENTO 5%'))
-print('-'*50)
-salario = float(input('Escreva o salário: R$'))
-novo_salario = salario + salario*0.15
-print('O novo salário com aumento de 15% é {:.2f}'.format(novo_salario))
--- 21-sorteioAluno.py
+++ 21-sorteioAluno.py
@@ -1,6 +1,9 @@
 '''
 Faça um sorteio entre 4 alunos.
 '''
+print('-'*50)
+print('{: ^50}'.format('SORTEIO DE ALUNOS'))
+print('-'*50)
 from random import choice
 aluno1 = input('Digite o nome do primeiro aluno: ')
 aluno2 = input('Digite o nome do segundo aluno: ')

7287f482f5323a618906ae03f16efb2c4608b92c
Danielle Farias <danielle8farias@gmail.com>
2018-10-16 21:43:24 -0300

    exercícios

---
+++ 65-aumentoSalario.py
@@ -0,0 +1,10 @@
+'Calcula o aumento de salário dado o valor e a porcentagem'
+print('-'*50)
+print('{: ^50}'.format('AUMENTO DE SALÁRIO'))
+print('-'*50)
+salario = float(input('Salário: R$ '))
+porcentagem = float(input('Porcentagem de aumento: '))
+aumento = salario * porcentagem/100
+salario = salario + aumento
+print('O aumento foi de R$ {:.2f}'.format(aumento))
+print('O novo salário é R$ {:.2f}'.format(salario))

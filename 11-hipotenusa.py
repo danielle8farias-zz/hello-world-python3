5c4af6a8ce4718e887adfedcc1b0bb959abbb03c
Danielle Farias <danielle8farias@gmail.com>
2018-10-21 22:56:59 -0300

    melhorias na aparência

--- 11-hipotenusa.py
+++ 11-hipotenusa.py
@@ -5,10 +5,22 @@ print('-'*50)
 print('{: ^50}'.format('CÁLCULO DA HIPOTENUSA'))
 print('-'*50)
 from math import sqrt
-b = float(input('Digite o primeiro cateto: '))
-c = float(input('Digite o segundo cateto: '))
-a = sqrt(b**2 + c**2)
-print('O valor da hipotenusa é: {}'.format(a))
+while True:
+    b = float(input('Digite o primeiro cateto: '))
+    c = float(input('Digite o segundo cateto: '))
+    a = sqrt(b**2 + c**2)
+    print()
+    print('O valor da hipotenusa é: {}'.format(a))
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)
 
 '''
 from math import hypot
--- 14-conversorDolar.py
+++ 14-conversorDolar.py
@@ -4,6 +4,18 @@ Faça um conversos de Real em Dolar.
 print('-'*50)
 print('{: ^50}'.format('CONVERSOR REAL EM DOLAR'))
 print('-'*50)
-real = float(input('Valor em Reais: R$'))
-dolar = real/3.86
-print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
+while True:
+    real = float(input('Valor em Reais: R$'))
+    dolar = real/3.86
+    print()
+    print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

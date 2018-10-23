a50f3e15229e9529b0cc53b1059cbb4e98d240e6
Danielle Farias <danielle8farias@gmail.com>
2018-10-23 12:21:11 -0300

    melhorias na aparência

--- 18-aluguelCarro.py
+++ 18-aluguelCarro.py
@@ -5,7 +5,18 @@ por dia e R$0,15 por quilômetro rodado.
 print('-'*50)
 print('{: ^50}'.format('PREÇO ALUGUEL DE CARRO'))
 print('-'*50)
-dias = int(input('Dias alugados: '))
-km = float(input('Quilometros rodados: '))
-preco = dias*60 + km*0.15
-print('O total a pagar foi R${:.2f}'.format(preco))
+while True:
+    dias = int(input('Dias alugados: '))
+    km = float(input('Quilometros rodados: '))
+    preco = dias*60 + km*0.15
+    print('O total a pagar foi R${:.2f}'.format(preco))
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

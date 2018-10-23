c136a22a3d6aec193d3d80947b5ff2f3c82aba68
Danielle Farias <danielle8farias@gmail.com>
2018-10-23 11:58:51 -0300

    melhorias na aparência

--- 16-descontoCinco.py
+++ 16-descontoCinco.py
@@ -4,6 +4,17 @@ Leia o valor e desconte 5%.
 print('-'*50)
 print('{: ^50}'.format('DESCONTO 5%'))
 print('-'*50)
-preco = float(input('Digite o preço: R$'))
-desconto = preco - (preco*0.05)
-print('O novo preço com desconto de 5% é R${:.2f}'.format(desconto))
+while True:
+    preco = float(input('Digite o preço: R$'))
+    desconto = preco - (preco*0.05)
+    print('O novo preço com desconto de 5% é R${:.2f}'.format(desconto))
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

8b3afd0aee48e527974db8b2d0bbdb12d6d01b8d
Danielle Farias <danielle8farias@gmail.com>
2018-10-23 11:49:36 -0300

    melhorias na aparência

--- 15-paredeTinta.py
+++ 15-paredeTinta.py
@@ -5,9 +5,20 @@ de tinta necessária para pintá-la, sabendo que cada litro pinta 2m².
 print('-'*50)
 print('{: ^50}'.format('CÁLCULO DA QUANTIDADE DE TINTA'))
 print('-'*50)
-largura = float(input('Informe a largura da parede: '))
-altura = float(input('Informe a altura da parede: '))
-area = largura*altura
-print('A área da parede é {}m².'.format(area))
-tinta = area/2
-print('A quantidade de tinta necessária é {:.2f} litros.'.format(tinta))
+while True:
+    largura = float(input('Informe a largura da parede: '))
+    altura = float(input('Informe a altura da parede: '))
+    area = largura*altura
+    print('A área da parede é {}m².'.format(area))
+    tinta = area/2
+    print('A quantidade de tinta necessária é {:.2f} litros.'.format(tinta))
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

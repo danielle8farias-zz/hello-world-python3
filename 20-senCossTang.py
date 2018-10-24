3d95791b232c55ce3edb74edadfda5d99da380ee
Danielle Farias <danielle8farias@gmail.com>
2018-10-24 17:52:43 -0300

    melhorias na aparência

--- 20-senCossTang.py
+++ 20-senCossTang.py
@@ -5,10 +5,21 @@ print('-'*50)
 print('{: ^50}'.format('SENO COSSENO TANGENTE'))
 print('-'*50)
 import math
-angulo = float(input('Digite um ângulo: '))
-seno = math.sin(math.radians(angulo))
-print('O seno é {:.2f}'.format(seno))
-cosseno = math.cos(math.radians(angulo))
-print('O cosseno é {:.2f}'.format(cosseno))
-tangente = math.tan(math.radians(angulo))
-print('A tangente é {:.2f}'.format(tangente))
+while True:
+    angulo = float(input('Digite um ângulo: '))
+    seno = math.sin(math.radians(angulo))
+    print('O seno é {:.2f}'.format(seno))
+    cosseno = math.cos(math.radians(angulo))
+    print('O cosseno é {:.2f}'.format(cosseno))
+    tangente = math.tan(math.radians(angulo))
+    print('A tangente é {:.2f}'.format(tangente))
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

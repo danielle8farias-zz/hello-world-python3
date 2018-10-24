5e13b092e5aff2722b013231b6549b8ffdee067c
Danielle Farias <danielle8farias@gmail.com>
2018-10-24 19:20:14 -0300

    melhorias na aparência

--- 22-sorteioLista.py
+++ 22-sorteioLista.py
@@ -2,12 +2,25 @@
 Faça um sorteio de apresentação de 4 alunos.
 '''
 print('-'*50)
+print('{: ^50}'.format('SORTEIO DE LISTA DE APRESENTAÇÃO'))
+print('-'*50)
 from random import shuffle
-aluno1 = input('Primeiro aluno: ')
-aluno2 = input('Segundo aluno: ')
-aluno3 = input('Terceiro aluno: ')
-aluno4 = input ('Quarto aluno: ')
-lista = [aluno1, aluno2, aluno3, aluno4]
-shuffle(lista)
-print('A ordem de apresentação será:')
-print(lista)
+while True:
+    aluno1 = input('Primeiro aluno: ')
+    aluno2 = input('Segundo aluno: ')
+    aluno3 = input('Terceiro aluno: ')
+    aluno4 = input ('Quarto aluno: ')
+    lista = [aluno1, aluno2, aluno3, aluno4]
+    shuffle(lista)
+    print('A ordem de apresentação será:')
+    print(lista)
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

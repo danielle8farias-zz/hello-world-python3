8371b66c7089e679424936c2c2081b6d90302089
Danielle Farias <danielle8farias@gmail.com>
2018-10-24 19:03:08 -0300

    melhorias na aparência

--- 21-sorteioAluno.py
+++ 21-sorteioAluno.py
@@ -5,10 +5,21 @@ print('-'*50)
 print('{: ^50}'.format('SORTEIO DE ALUNOS'))
 print('-'*50)
 from random import choice
-aluno1 = input('Digite o nome do primeiro aluno: ')
-aluno2 = input('Digite o nome do segundo aluno: ')
-aluno3 = input('Digite o nome do terceiro aluno: ')
-aluno4 = input('Digite o nome do quarto aluno: ')
-lista = [aluno1,aluno2,aluno3,aluno4]
-escolhido = choice(lista)
-print('O escolhido foi {}'.format(escolhido))
+while True:
+    aluno1 = input('Digite o nome do primeiro aluno: ')
+    aluno2 = input('Digite o nome do segundo aluno: ')
+    aluno3 = input('Digite o nome do terceiro aluno: ')
+    aluno4 = input('Digite o nome do quarto aluno: ')
+    lista = [aluno1,aluno2,aluno3,aluno4]
+    escolhido = choice(lista)
+    print('O escolhido foi {}'.format(escolhido))
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

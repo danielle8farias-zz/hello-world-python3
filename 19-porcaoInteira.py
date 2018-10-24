3ffc90ff844badc189cbf9a3dbe5eecee178eb5e
Danielle Farias <danielle8farias@gmail.com>
2018-10-24 17:49:43 -0300

    melhorias na aparência

--- 19-porcaoInteira.py
+++ 19-porcaoInteira.py
@@ -1,9 +1,21 @@
 '''
-Leia um número real qualquer e retorne apenas a parte inteira.
+Leia um número real qualquer e retorne apenas a parte inteira,
+sem arredondamento.
 '''
 print('-'*50)
 print('{: ^50}'.format('PARTE INTEIRA DE UM NÚMERO REAL'))
 print('-'*50)
 from math import trunc
-num = float(input('Digite um número real: '))
-print('A parte inteira do valor digitado foi {}.'.format(trunc(num)))
+while True:
+    num = float(input('Digite um número real: '))
+    print('A parte inteira do valor digitado foi {}.'.format(trunc(num)))
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

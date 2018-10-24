40506c0a462d7a0fa542796dbf70adc89a77d4eb
Danielle Farias <danielle8farias@gmail.com>
2018-10-24 20:49:28 -0300

    melhorias na aparência

--- 28-mediaBoaRuim.py
+++ 28-mediaBoaRuim.py
@@ -2,11 +2,25 @@
 Informe duas notas de um aluno. Se a média for maior ou igual a 6,
 a média é boa. Senão, a média é ruim.
 '''
-nota1 = float(input('Digite a primeira nota: '))
-nota2 = float(input('Digite a segunda nota: '))
-media = (nota1 + nota2)/2
-print('A sua média foi {:.1f}'.format(media))
-if media >= 6:
-    print('Sua média foi boa. Parabéns!!!')
-else:
-    print('Sua média foi ruim. Estude mais!')
+print('-'*50)
+print('{: ^50}'.format('MÉDIA ENTRE DUAS NOTAS'))
+print('-'*50)
+while True:
+    nota1 = float(input('Digite a primeira nota: '))
+    nota2 = float(input('Digite a segunda nota: '))
+    media = (nota1 + nota2)/2
+    print('A sua média foi {:.1f}'.format(media))
+    if media >= 6:
+        print('Sua média foi boa. Parabéns!!!')
+    else:
+        print('Sua média foi ruim. Estude mais!')
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

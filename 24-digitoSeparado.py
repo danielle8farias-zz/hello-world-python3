27969463c5fc3a28411f78e256f3735aefe0c48d
Danielle Farias <danielle8farias@gmail.com>
2018-10-24 19:52:10 -0300

    melhorias na aparência

--- 24-digitoSeparado.py
+++ 24-digitoSeparado.py
@@ -1,8 +1,10 @@
 '''
 Leia um número de 0 a 9999 e mostre cada um dos dígitos.
 '''
+print('-'*50)
+print('{: ^50}'.format('DÍGITOS DE UM NÚMERO'))
+print('-'*50)
 num = int(input("Digite um número: "))
-
 if 0 < num <= 9999:
     mil = num //1000
     resto_m = num -(mil*1000)
@@ -10,27 +12,22 @@ if 0 < num <= 9999:
         print(mil,'milhar')
     elif mil != 0:
         print(mil,'milhares')
-
     cent = resto_m//100
     resto_c = resto_m - (cent*100)
     if cent == 1:
         print(cent,"centena")
     elif cent !=0:
         print(cent,"centenas")
-
     deze = resto_c//10
     resto_d = resto_c - (deze*10)
     if deze == 1:
         print(deze,"dezena")
     elif deze !=0:
         print(deze,"dezenas")
-
     unid = resto_d//1
     if unid == 1:
         print(unid,"unidade")
     elif unid !=0:
         print(unid,"unidades")
-
-
 else:
     print("Valor inválido! Escolha um número maior ou igual a 1 e menor que 10000.")
--- 25-digitoSeparado.py
+++ 25-digitoSeparado.py
@@ -1,12 +1,26 @@
 '''
 Leia um número de 0 a 9999 e mostre cada um dos dígitos.
 '''
-num = int(input('Digite um número: '))
-u = num // 1 % 10
-d = num // 10 % 10
-c = num // 100 % 10
-m = num // 1000 % 10
-print('Unidade: {}'.format(u))
-print('Dezena: {}'.format(d))
-print('Centena: {}'.format(c))
-print('Milhar: {}'.format(m))
+print('-'*50)
+print('{: ^50}'.format('DÍGITOS DE UM NÚMERO'))
+print('-'*50)
+while True:
+    num = int(input('Digite um número: '))
+    u = num // 1 % 10
+    d = num // 10 % 10
+    c = num // 100 % 10
+    m = num // 1000 % 10
+    print('Unidade: {}'.format(u))
+    print('Dezena: {}'.format(d))
+    print('Centena: {}'.format(c))
+    print('Milhar: {}'.format(m))
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+print('-'*50)
+print('{: ^50}'.format('FIM'))
+print('-'*50)

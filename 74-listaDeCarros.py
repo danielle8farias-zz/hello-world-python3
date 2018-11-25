0854df048fead6282bfe7b534fe23c52156e8dab
Danielle Farias <danielle8farias@gmail.com>
2018-11-25 17:37:36 -0300

    numa lista de carros, retorna a terceira posição

---
+++ 74-listaDeCarros.py
@@ -0,0 +1,11 @@
+'''
+Crie uma lista com marcas de carros e depois acesse o terceiro item.
+'''
+print('-'*50)
+print(f'{"LISTA DE CARROS":^50}')
+print('-'*50)
+carros = ['FIAT', 'Ford','BMW', 'Jeep','Tesla']
+print('O terceiro carro é: {}'.format(carros[2]))
+print('-'*50)
+print('{:^50}'.format('FIM'))
+print('-'*50)

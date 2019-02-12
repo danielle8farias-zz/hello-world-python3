fcaf49af41e76e57db38caf74d725e3c882d1f74
Danielle Farias <danielle8farias@gmail.com>
2019-02-12 12:08:31 -0300

    usando funções

--- 2-diaMesAnoNascimento.py
+++ 2-diaMesAnoNascimento.py
@@ -1,10 +1,15 @@
 '''
 Pedir ao usuário para digitar a data completa do nascimento.
 '''
-print('Olá!')
-print('-'*50)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('data completa do nascimento')
+
 dia = input('Digite o dia do seu nascimento: ').upper().strip()
 mes = input('Digite o mês do seu nascimento: ').upper().strip()
 ano = input('Digite o ano do seu nascimento: ').upper().strip()
-print('-'*50)
+print()
 print('Você nasceu no dia {} de {} de {}.'.format(dia, mes, ano))
+
+rodape()

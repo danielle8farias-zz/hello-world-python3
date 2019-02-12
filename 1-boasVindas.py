e8cd92309d53c516d8d9693a3ead2733fd9a8203
Danielle Farias <danielle8farias@gmail.com>
2019-02-12 12:03:04 -0300

    usando funções

--- 1-boasVindas.py
+++ 1-boasVindas.py
@@ -1,6 +1,13 @@
 '''
 Leia um nome e mostre uma mensagem de boas-vindas
 '''
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('mensagem de boas vindas')
+
 nome = input('Qual o seu nome? ').upper().strip()
-print('='*50)
+print()
 print('Seja bem-vinda(o), {}!'.format(nome))
+
+rodape()

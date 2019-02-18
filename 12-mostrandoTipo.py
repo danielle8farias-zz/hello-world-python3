38dc9d86febbbcf10c77dcfc242ea81b77b7011f
Danielle Farias <danielle8farias@gmail.com>
2019-02-18 20:13:20 -0300

    funções de cabeçalho, rodapé e linha

--- 12-mostrandoTipo.py
+++ 12-mostrandoTipo.py
@@ -1,15 +1,27 @@
 '''
 Digite algo e mostre a tipagem da mesma.
 '''
-print('-'*50)
-print('{: ^50}'.format('TIPO DE DADO'))
-print('-'*50)
-a = input('Digite algo: ')
-print('O tipo primitivo desse valor é', type(a))
-print('Só tem espaços?', a.isspace())
-print('É um número?', a.isnumeric())
-print('É composto de letras?', a.isalpha())
-print('Ele é alfanumérico?', a.isalnum())
-print('Está em maiúsculas?', a.isupper())
-print('Está em minúsculas?', a.islower())
-print('Está capitalizada?', a.istitle())
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('TIPO DE DADO')
+
+while True:
+    a = input('Digite algo: ')
+    print('O tipo primitivo desse valor é', type(a))
+    print('Só tem espaços?', a.isspace())
+    print('É um número?', a.isnumeric())
+    print('É composto de letras?', a.isalpha())
+    print('Ele é alfanumérico?', a.isalnum())
+    print('Está em maiúsculas?', a.isupper())
+    print('Está em minúsculas?', a.islower())
+    print('Está capitalizada?', a.istitle())
+    print()
+    resposta = ' '
+    while resposta not in 'SN':
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if resposta == 'N':
+        break
+    print()
+
+rodape()

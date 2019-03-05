8d830b3e401fbe4b441addb44ad2d1837e4fa597
Danielle Farias <danielle8farias@gmail.com>
2019-03-05 19:46:38 -0300

    funções de cabeçalho, rodapé e linha

--- 48-somaImparMultiploDeTres.py
+++ 48-somaImparMultiploDeTres.py
@@ -1,12 +1,16 @@
 '''
 Soma dos números ímpares múltiplos de 3 até 500.
 '''
-print('-'*50)
-print('{: ^50}'.format('Soma dos números ímpares múltiplos de 3 até 500'))
-print('-'*50)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('Soma dos números ímpares múltiplos de 3 até 500')
+
 soma = 0
 for n in range(1, 500, 2):
     if n % 3 == 0:
         print(n, end=' ')
         soma += n
 print('\nA soma desses números é {}.'.format(soma))
+
+rodape()

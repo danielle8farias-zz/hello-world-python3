cbf64dc616a1b7506e9bc84cedcc1e1a69337ed2
Danielle Farias <danielle8farias@gmail.com>
2019-03-05 19:45:36 -0300

    funções de cabeçalho, rodapé e linha

--- 46-contagemRegressiva.py
+++ 46-contagemRegressiva.py
@@ -1,11 +1,15 @@
 '''
 Fazer uma contagem regressiva de 10 a 0.
 '''
+from mensagem import cabecalho
+from mensagem import rodape
 from time import sleep
-print('-'*50)
-print('{: ^50}'.format('CONTAGEM REGRESSIVA'))
-print('-'*50)
+
+cabecalho('CONTAGEM REGRESSIVA')
+
 for cont in range(10, -1, -1):
     print(cont)
     sleep(1)
 print('BOM! BOOM!! POWW!!!')
+
+rodape()

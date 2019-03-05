84d28ebd5f0c798e35bc38c785bfd882d841368d
Danielle Farias <danielle8farias@gmail.com>
2019-03-05 19:45:58 -0300

    funções de cabeçalho, rodapé e linha

--- 47-contagemPares.py
+++ 47-contagemPares.py
@@ -1,8 +1,13 @@
 '''
 Contagem de números pares até 50.
 '''
-print('-'*50)
-print('{: ^50}'.format('CONTAGEM Nº PARES ATÉ 50'))
-print('-'*50)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('CONTAGEM Nº PARES ATÉ 50')
+
 for i in range(2, 51, 2):
     print(i, end=' ')
+print()
+
+rodape()

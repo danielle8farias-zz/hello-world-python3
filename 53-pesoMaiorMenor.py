9751c7acb8eb5330978ebb2fe891582df519bdfb
Danielle Farias <danielle8farias@gmail.com>
2019-03-07 18:11:59 -0300

    funções de cabeçalho, rodapé e linha

--- 53-pesoMaiorMenor.py
+++ 53-pesoMaiorMenor.py
@@ -1,9 +1,11 @@
 '''
 Leia o peso de 5 pessoas. Mostre o maior e o menor peso lido.
 '''
-print('='*35)
-print('INFORMANDO O MAIOR E O MENOR PESO')
-print('='*35)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('INFORMANDO O MAIOR E O MENOR PESO')
+
 maior = 0
 menor = 0
 for n in range(1,6):
@@ -18,3 +20,5 @@ for n in range(1,6):
             menor = peso
 print('O maior peso é {}Kg.'.format(maior))
 print('O menor peso é {}Kg.'.format(menor))
+
+rodape()

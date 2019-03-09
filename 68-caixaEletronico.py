053634adb725ad5c4510f83f9472bb8763371ed0
Danielle Farias <danielle8farias@gmail.com>
2019-03-09 00:27:34 -0300

    funções de cabeçalho, rodapé e linha

--- 68-caixaEletronico.py
+++ 68-caixaEletronico.py
@@ -3,9 +3,11 @@ Crie um caixa eletrônico. Pergunte ao usuário qual valor a ser sacado (número
 inteiro) e o programa vai informar quantas cédulas de cada valor serão
 entregues. O caixa possui cédulas de 50, 20, 10 e 1.
 '''
-print('-'*50)
-print('{: ^50}'.format('CAIXA ELETRÔNICO'))
-print('-'*50)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('CAIXA ELETRÔNICO')
+
 valor = int(input('Valor a ser sacado: R$'))
 if valor >= 50:
     notas = valor//50
@@ -23,6 +25,5 @@ if valor >= 1:
     notas = valor//1
     valor = valor % 1
     print('{} notas de R$1'.format(notas))
-print('-'*50)
-print('{: ^50}'.format('FIM'))
-print('-'*50)
+
+rodape()

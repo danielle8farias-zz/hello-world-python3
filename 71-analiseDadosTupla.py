edac886fe3024ed1f35912c69c259660e3e4579b
Danielle Farias <danielle8farias@gmail.com>
2019-03-10 18:12:32 -0300

    funções de cabeçalho, rodapé e linha

--- 71-analiseDadosTupla.py
+++ 71-analiseDadosTupla.py
@@ -4,23 +4,25 @@ Leia quatro valores pelo teclado e armazene em uma tupla. Verifique:
 - posição do primeiro valor 3;
 - quais foram os números pares.
 '''
-print('-'*50)
-print('{: ^50}'.format('ANÁLISE DE DADOS EM UMA TUPLA'))
-print('-'*50)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('ANÁLISE DE DADOS EM UMA TUPLA')
+
 num = (int(input('Digite o 1º número: ')),
         int(input('Digite o 2º número: ')),
         int(input('Digite o 3º número: ')),
         int(input('Digite o 4º número: ')))
-print('Os números digitados foram: {}'.format(num))
-print('O valor 9 apareceu {} vezes.'.format(num.count(9)))
+print(f'Os números digitados foram: {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes.')
 if 3 in num:
-    print('O valor 3 apareceu na {}ª posição.'.format(num.index(3)))
+    print(f'O valor 3 apareceu na {num.index(3)+1}ª posição.')
 else:
     print('Não há número 3.')
 print('Os valores pares são: ',end='')
 for i in num:
     if i % 2 == 0:
         print(i, end=' - ')
-print('-'*50)
-print('{: ^50}'.format('FIM'))
-print('-'*50)
+print()
+
+rodape()

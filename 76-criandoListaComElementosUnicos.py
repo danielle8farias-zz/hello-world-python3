7e882a9d37bee2a3e22a34bdb64d5d438eb6d0dc
Danielle Farias <danielle8farias@gmail.com>
2019-03-11 20:40:23 -0300

    funções de cabeçalho, rodapé e linha

--- 76-criandoListaComElementosUnicos.py
+++ 76-criandoListaComElementosUnicos.py
@@ -3,9 +3,11 @@ Crie um programa onde o usuário possa digitar vários inteiros e guarde-os em
 uma lista. Caso o número já tenha sido digitado, este não será adicionado.
 Retorne a lista com os valores em ordem crescente.
 '''
-print('-'*50)
-print('{:^50}'.format('LISTA COM VALORES ÚNICOS EM ORDEM CRESCENTE'))
-print('-'*50)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('LISTA COM VALORES ÚNICOS EM ORDEM CRESCENTE')
+
 valores = []
 while True:
     valor = int(input('Digite um valor: '))
@@ -25,6 +27,5 @@ while True:
         if resposta == 'N':
             break
 print('Os valores digitados foram: {}'.format(sorted(valores)))
-print('-'*50)
-print('{:^50}'.format('FIM'))
-print('-'*50)
+
+rodape()

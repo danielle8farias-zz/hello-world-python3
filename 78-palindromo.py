c115eedecb913be949b3980efdb1cc30e113c6fb
Danielle Farias <danielle8farias@gmail.com>
2019-03-14 12:01:06 -0300

    funções de cabeçalho, rodapé e linha

--- 78-palindromo.py
+++ 78-palindromo.py
@@ -1,17 +1,25 @@
 '''
 Leia uma frase e verique se é palíndromo.
 '''
-print('-'*50)
-print(f'{"VERIICA SE UMA FRASE É PALÍNDROMO":^50}')
-print('-'*50)
-frase = input('Digite uma frase: ').strip().lower()
-palavras = frase.split() #divide uma string em uma lista
-junto = ''.join(palavras) #junta listas strings separada pelo caractere ''
-print(junto)
-if junto == junto[::-1]:
-    print('É um palíndromo!')
-else:
-    print('Não é palíndromo')
-print('-'*50)
-print(f'{"FIM":^50}')
-print('-'*50)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('VERIFICA SE UMA FRASE É PALÍNDROMO')
+
+while True:
+    frase = input('Digite uma frase: ').strip().lower()
+    palavras = frase.split() #divide uma string em uma lista
+    junto = ''.join(palavras) #junta listas strings separada pelo caractere ''
+    print(junto)
+    if junto == junto[::-1]:
+        print('É um palíndromo!')
+    else:
+        print('Não é palíndromo')
+    print()
+    resp = ' '
+    while resp not in 'SN':
+        resp = input('Deseja continuar? [S/N] ').strip().upper()[0]
+    if resp == 'N':
+        break
+    print()
+rodape()

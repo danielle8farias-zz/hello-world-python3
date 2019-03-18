7e57b8c41358c8279cb76a3d124c62296cee209a
Danielle Farias <danielle8farias@gmail.com>
2019-03-18 19:49:30 -0300

    funções de cabeçalho, rodapé e linha

--- 80-palavraTrocaVogal.py
+++ 80-palavraTrocaVogal.py
@@ -2,17 +2,18 @@
 Faça um programa que leia uma palavra e troque as vogais por "*" (asterísco).
 Exercício 77 com utilizando o "for".
 '''
-print('-'*50)
-print(f'{"TROCA VOGAIS POR ASTERÍSCO":^50}')
-print('-'*50)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('TROCA VOGAIS POR ASTERÍSCO')
+
 palavra = input('Escreva uma palavra: ').strip().lower()
 troca = ''
-for vogal in range(0,len(palavra)):
+for vogal in range(len(palavra)):
     if palavra[vogal] in 'aeiou':
         troca += '*'
     else:
         troca += palavra[vogal]
 print(f'A nova palavra é: {troca}')
-print('-'*50)
-print(f'{"FIM":^50}')
-print('-'*50)
+
+rodape()

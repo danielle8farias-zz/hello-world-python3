77b19d828fdbe0474bfa28b6256c38a0d771f6e0
Danielle Farias <danielle8farias@gmail.com>
2019-03-25 15:25:42 -0300

    funções de cabeçalho, rodapé e linha

--- 90-conversaoTemperatura.py
+++ 90-conversaoTemperatura.py
@@ -2,17 +2,20 @@
 Crie um programa que faça a conversão de temperaturas de Celsius para Farenheit.
 E de Farenheit para Celsius.
 '''
-print('-'*50)
-print('{: ^50}'.format('CONVERSÃO DE TEMPERATURAS'))
-print('-'*50)
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('CONVERSÃO DE TEMPERATURAS')
 
 def cf(valor):
     f = ( (valor*9) / 5 ) + 32
     print(f'A temperatura em graus Farenheit é {f:.2f}')
 
+
 def fc(valor):
     c = (5 * (valor - 32) / 9)
     print(f'A temperatura em graus Celsius é {c:.2f}')
+
 
 resposta = ' '
 while resposta not in '12':
@@ -26,6 +29,4 @@ elif resposta == '2':
     valor = float(input('Digite a temperatura em Farenheit: '))
     fc(valor)
 
-print('-'*50)
-print('{: ^50}'.format('FIM'))
-print('-'*50)
+rodape()

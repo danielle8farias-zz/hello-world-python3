aa97152b92db164f6f93c8bf62d62dda88c2186a
Danielle Farias <danielle8farias@gmail.com>
2019-03-30 22:18:24 -0300

    ranking de um jogo de dados com uso de dicionários

---
+++ 103-rankingDados.py
@@ -0,0 +1,27 @@
+'''
+Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios.
+Guarde esses resultados num dicionário. Ao final, coloque esse dicionário em ordem,
+sabendo que o vencedor tirou o maior número no dado.
+'''
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+jogo = { 'jogador1': randint(1,6),
+         'jogador2': randint(1,6),
+         'jogador3': randint(1,6),
+         'jogador4': randint(1,6)
+        }
+
+print('Valores sorteados:')
+for k,v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+ranking = []
+ranking = sorted(jogo.items(), key = itemgetter(1), reverse = True)
+print('-'*50)
+for i,v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}')
+    sleep(1)
+print('-'*50)
+print(f'{"FIM":^50}')

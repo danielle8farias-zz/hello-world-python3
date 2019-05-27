d3e36d3e0d14806c92ab85c4c5bb020edaae5d9c
Danielle Farias <danielle8farias@gmail.com>
2019-05-27 14:35:39 -0300

    adicionando comentários

--- 1-boasVindas.py
+++ 1-boasVindas.py
@@ -5,12 +5,16 @@ Leia um nome e mostre uma mensagem de boas-vindas
 from mensagem import cabecalho
 from mensagem import rodape
 
+#chamando a importação
 cabecalho('mensagem de boas vindas')
 
+#função
 def nome():
     nome = input('Qual o seu nome? ').upper().strip()
     print(f'Seja bem-vindo {nome}!')
 
+#chamando função principal
 nome()
 
+#chamando a importação
 rodape()
--- 2-diaMesAnoNascimento.py
+++ 2-diaMesAnoNascimento.py
@@ -1,15 +1,28 @@
 '''
 Pedir ao usuário para digitar a data completa do nascimento.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
+#chamando a importação
 cabecalho('data completa do nascimento')
 
-dia = input('Digite o dia do seu nascimento: ').upper().strip()
+def dia():
+    while True:
+        #limitando o dia de 1 a 31
+        dia = input('Digite o dia do seu nascimento: ').upper().strip()
+        if 0 > dia < 32:
+            return dia
+            break
+        else:
+            print('Digite uma data válida!')
+
+
 mes = input('Digite o mês do seu nascimento: ').upper().strip()
 ano = input('Digite o ano do seu nascimento: ').upper().strip()
 print()
-print('Você nasceu no dia {} de {} de {}.'.format(dia, mes, ano))
+print(f'Você nasceu no dia {dia()} de {mes} de {ano}.')
 
+#chamando a importação
 rodape()

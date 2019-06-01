951c73798da39a329f8200d2d6b4eb5567ef59be
Danielle Farias <danielle8farias@gmail.com>
2019-06-01 18:55:18 -0300

    usando funções

--- 9-numAleatorio.py
+++ 9-numAleatorio.py
@@ -6,13 +6,22 @@ from mensagem import cabecalho
 from mensagem import rodape
 import random
 
-cabecalho('NÚMEROS ALEATÓRIOS')
-
-while True:
+#funções
+def num_aleatorio1():
     num = random.random()
-    num2 = random.randint(1,10)
+    return num
+
+def num_aleatorio2():
+    num = random.randint(1,10)
+    return num
+
+#programa principal
+cabecalho('NÚMEROS ALEATÓRIOS')
+while True:
+    num1 = num_aleatorio1()
+    num2 = num_aleatorio2()
     print('Primeiro número aleatório entre 0 e 1')
-    print(num)
+    print(num1)
     print('Segundo número aleatório entre 1 e 10')
     print(num2)
     print()
@@ -22,5 +31,4 @@ while True:
     if resposta == 'N':
         break
     print()
-
 rodape()

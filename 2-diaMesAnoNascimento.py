69b81b2045fc1dd331f64f7f3c21578ee9d932f1
Danielle Farias <danielle8farias@gmail.com>
2019-06-04 17:39:42 -0300

    adicionando comentários e loop

--- 2-diaMesAnoNascimento.py
+++ 2-diaMesAnoNascimento.py
@@ -6,7 +6,7 @@ from mensagem import cabecalho
 from mensagem import rodape
 from datetime import date
 
-# início funções
+#função que captura o dia informado pelo usuário
 def dia():
     while True:
         dia = int(input('Digite o dia do seu nascimento: '))
@@ -17,7 +17,7 @@ def dia():
         else:
             print('Digite uma data válida!')
 
-
+#função que captura o mes informado pelo usuário
 def mes():
     while True:
         mes = int(input('Digite o mês do seu nascimento: '))
@@ -28,9 +28,10 @@ def mes():
         else:
             print('Digite uma data válida!')
 
-
+#função que captura o ano informado pelo usuário
 def ano():
     while True:
+        #date.today().year pega o ano indicado pelo sistema operacional
         # retirando 5 anos do ano atual
         ano_atual = date.today().year - 5
         ano = int(input('Digite o ano do seu nascimento: '))
@@ -40,13 +41,25 @@ def ano():
             break
         else:
             print('Digite uma data válida!')
-#fim das funções
 
 #programa principal
 cabecalho('data completa do nascimento')
-dia_info = dia()
-mes_info = mes()
-ano_info = ano()
-print()
-print(f'Você nasceu no dia {dia_info} de {mes_info} de {ano_info}.')
+#1º laço fazendo o programa rodar até que o usuário decida parar
+while True:
+    dia_info = dia()
+    mes_info = mes()
+    ano_info = ano()
+    print(f'Você nasceu no dia {dia_info} de {mes_info} de {ano_info}.')
+    print()
+    resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
+    while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
+        resposta = input('Deseja continuar? [S/N]? ').upper().strip()[0]
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
+    print()
 rodape()

f49f6283e31bd912ca26f3006ce7ffbc1d6b87ba
Danielle Farias <danielle8farias@gmail.com>
2019-06-04 17:43:06 -0300

    usando funções

--- 17-salarioAumentoQuinze.py
+++ 17-salarioAumentoQuinze.py
@@ -4,12 +4,17 @@ Leia o salário e o aumente em 15%.
 from mensagem import cabecalho
 from mensagem import rodape
 
-cabecalho('SALÁRIO AUMENTO 15%')
+#função
+def novo_salario(salario):
+    novo_salario = salario + salario*0.15
+    return novo_salario
 
+#programa principal
+cabecalho('SALÁRIO AUMENTO 15%')
 while True:
     salario = float(input('Escreva o salário: R$'))
-    novo_salario = salario + salario*0.15
-    print('O novo salário com aumento de 15% é {:.2f}'.format(novo_salario))
+    valor_salario = novo_salario(salario)
+    print(f'O novo salário com aumento de 15% é {valor_salario:.2f}')
     print()
     resposta = ' '
     while resposta not in 'SN':
@@ -17,5 +22,4 @@ while True:
     if resposta == 'N':
         break
     print()
-
 rodape()

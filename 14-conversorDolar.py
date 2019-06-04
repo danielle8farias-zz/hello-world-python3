a781359935b672e3cd9a40ba016e66981e3583cb
Danielle Farias <danielle8farias@gmail.com>
2019-06-04 17:40:33 -0300

    usando funções

--- 14-conversorDolar.py
+++ 14-conversorDolar.py
@@ -4,13 +4,17 @@ Faça um conversos de Real em Dolar.
 from mensagem import cabecalho
 from mensagem import rodape
 
-cabecalho('CONVERSOR REAL EM DOLAR')
+#função
+def dollar(real):
+    dolar = real/3.89 #em 03/06/2019
+    return dolar
 
+#programa principal
+cabecalho('CONVERSOR REAL EM DOLAR')
 while True:
     real = float(input('Valor em Reais: R$'))
-    dolar = real/3.86
-    print()
-    print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
+    valor_dolar = dollar(real)
+    print(f'Com R${real:.2f} você pode comprar US${valor_dolar:.2f}')
     print()
     resposta = ' '
     while resposta not in 'SN':
@@ -18,5 +22,4 @@ while True:
     if resposta == 'N':
         break
     print()
-
 rodape()

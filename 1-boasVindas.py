61831e72216f08126bfbb5de4ea4f77074750227
Danielle Farias <danielle8farias@gmail.com>
2019-06-03 22:10:04 -0300

    adicionando comentários

--- 1-boasVindas.py
+++ 1-boasVindas.py
@@ -5,13 +5,21 @@ Leia um nome e mostre uma mensagem de boas-vindas
 from mensagem import cabecalho
 from mensagem import rodape
 
-#função
-def nome():
-    nome = input('Qual o seu nome? ').upper().strip()
-    return nome
-
 #programa principal
 cabecalho('mensagem de boas vindas')
-nome_info = nome()
-print(f'Seja bem-vindo {nome_info}!')
+#laço fazendo o programa rodar até que o usuário decida parar
+while True:
+    #upper: joga a string para maiúsculo
+    #strip: remove os espaços no começo e no fim
+    nome = input('Qual o seu nome? ').upper().strip()
+    print(f'Seja bem-vindo {nome}!')
+    print()
+    resposta = ' '
+    #laço enquanto a resposta não for S ou N
+    while resposta not in 'SN':
+        #[0] captura apenas o primeiro caractere
+        resposta = input('Deseja continuar? [S/N]? ').upper().strip()[0]
+    if resposta == 'N':
+        #quebrando o laço
+        break
 rodape()

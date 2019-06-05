dd69bf2ec631bc2c9b54cde733bae4a37e81ace4
Danielle Farias <danielle8farias@gmail.com>
2019-06-05 20:19:28 -0300

    adicionando comentários

--- 5-dobroTriploRaizQuadrada.py
+++ 5-dobroTriploRaizQuadrada.py
@@ -1,25 +1,28 @@
 '''
 Digite um número e mostro o dobro, o triplo e a raiz quadrada.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
-#funções
+#função que calcula o dobro do número informado
 def dobro(num):
     dobro = num*2
     return dobro
 
-
+#função que calcula o triplo do número informado
 def triplo(num):
     triplo = num*3
     return triplo
 
+#função que calcula a raiz do número informado
 def raiz(num):
     raiz = num**(1/2)
     return raiz
 
 #programa principal
 cabecalho('DOBRO, TRIPLO E RAÍZ QUADRADA')
+#1º laço fazendo o programa rodar até que o usuário decida parar
 while True:
     num = float(input('Digite um número: '))
     dobro_info = dobro(num)
@@ -30,9 +33,14 @@ while True:
     print(f'A raiz quadrada é: {raiz_info}')
     print()
     resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
     while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
         resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
     print()
     if resposta == 'N':
+        #quebrando o 1º laço
         break
 rodape()

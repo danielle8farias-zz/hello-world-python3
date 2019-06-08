f844bd690fda74c221fb357d4dfb0771dfc60857
Danielle Farias <danielle8farias@gmail.com>
2019-06-08 13:16:30 -0300

    adicionando comentários

--- 12-mostrandoTipo.py
+++ 12-mostrandoTipo.py
@@ -1,10 +1,13 @@
 '''
 Digite algo e mostre a tipagem da mesma.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
+#programa principal
 cabecalho('TIPO DE DADO')
+#1º laço fazendo o programa rodar até que o usuário decida parar
 while True:
     a = input('Digite algo: ')
     print('O tipo primitivo desse valor é', type(a))
@@ -17,9 +20,14 @@ while True:
     print('Está capitalizada?', a.istitle())
     print()
     resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
     while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
         resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if resposta == 'N':
-        break
     print()
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
 rodape()

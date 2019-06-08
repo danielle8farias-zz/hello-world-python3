72c0c3032e41b0d188357a34547180ac77e07295
Danielle Farias <danielle8farias@gmail.com>
2019-06-08 13:16:40 -0300

    adicionando comentários

--- 13-tabuadaMultiplicacao.py
+++ 13-tabuadaMultiplicacao.py
@@ -2,16 +2,22 @@
 Faça a tabuada de multiplicação de vários números, um de cada vez.
 O programa para quando é dado um número negativo.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
+#programa principal
 cabecalho('TABUADA')
 print('Para encerrar digite um número negativo')
 num = int(input('Digite um número para ver sua tabuada: '))
+#laço fazendo o programa rodar até que o usuário decida parar
 while num >= 0:
     print('-'*40)
+    #laço que vai de 0 a 10
     for i in range (0,11):
         multiplicacao = num * i
-        print('{} * {:2} = {}'.format(num, i, multiplicacao))
+        print(f'{num} * {i:2} = {multiplicacao}')
+    print()
+    print('Para encerrar digite um número negativo')
     num = int(input('Digite um número para ver sua tabuada: '))
 rodape()

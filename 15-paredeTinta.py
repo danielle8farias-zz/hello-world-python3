06bd20d972532bbbfe9812a901a516fb24dcde05
Danielle Farias <danielle8farias@gmail.com>
2019-06-08 13:17:26 -0300

    adicionando comentários

--- 15-paredeTinta.py
+++ 15-paredeTinta.py
@@ -2,33 +2,42 @@
 Leia a altura e largura da parede em metros e calcule a área e a quantidade
 de tinta necessária para pintá-la, sabendo que cada litro pinta 2m².
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
-#funções
+#função que calcula a área
 def area(largura,altura):
     area = largura*altura
     return area
 
-
+#função que calcula a quantidade de tinta
 def quant_tinta(valor_area):
     quant_tinta = valor_area/2
     return quant_tinta
 
 #programa principal
 cabecalho('CÁLCULO DA QUANTIDADE DE TINTA')
+#1º laço fazendo o programa rodar até que o usuário decida parar
 while True:
     largura = float(input('Informe a largura da parede: '))
     altura = float(input('Informe a altura da parede: '))
     valor_area = area(largura,altura)
-    print(f'A área da parede é {valor_area}m².')
-    tinta = quant_tinta(valor_area)    
+    #:.2f limita o número de duas casas decimais
+    print(f'A área da parede é {valor_area:.2f}m².')
+    tinta = quant_tinta(valor_area)
+    #:.2f limita o número de duas casas decimais
     print(f'A quantidade de tinta necessária é {tinta:.2f} litros.')
     print()
     resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
     while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
         resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if resposta == 'N':
-        break
     print()
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
 rodape()

55943fb6c3e6904ec694feeec72449a8e9475a4f
Danielle Farias <danielle8farias@gmail.com>
2019-06-10 18:00:24 -0300

    adicionando comentários

--- 20-senCossTang.py
+++ 20-senCossTang.py
@@ -1,41 +1,48 @@
 '''
 Informe um ângulo e calcule o valor do seno, cosseno e tangente.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 from math import radians
+from math import sin
+from math import cos
+from math import tan
 
-cabecalho('SENO COSSENO TANGENTE')
-
+#função que calcula o seno
 def seno(angulo):
-    from math import sin
     seno = sin(radians(angulo))
     return seno
 
-
+#função que calcula o cosseno
 def cosseno(angulo):
-    from math import cos
     cosseno = cos(radians(angulo))
     return cosseno
 
-
+#função que calcula a tangente
 def tangente(angulo):
-    from math import tan
     tangente = tan(radians(angulo))
     return tangente
-    
-    
+
+#programa principal
+cabecalho('SENO COSSENO TANGENTE')
+#1º laço fazendo o programa rodar até que o usuário decida parar
 while True:
     angulo = float(input('Digite um ângulo: '))
-    print('O seno é {:.2f}'.format(seno(angulo)))
-    print('O cosseno é {:.2f}'.format(cosseno(angulo)))
-    print('A tangente é {:.2f}'.format(tangente(angulo)))
+    #:.2f limita o número de duas casas decimais
+    print(f'O seno é {seno(angulo):.2f}')
+    print(f'O cosseno é {cosseno(angulo):.2f}')
+    print(f'A tangente é {tangente(angulo):.2f}')
     print()
     resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
     while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
         resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if resposta == 'N':
-        break
     print()
-
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
 rodape()

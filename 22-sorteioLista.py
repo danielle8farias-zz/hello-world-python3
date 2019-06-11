1b5ab5f3ca224d6e45a4528ca0e623cce56da30e
Danielle Farias <danielle8farias@gmail.com>
2019-06-11 17:02:44 -0300

    adicionando comentários

--- 22-sorteioLista.py
+++ 22-sorteioLista.py
@@ -1,27 +1,32 @@
 '''
 Faça um sorteio de apresentação de 4 alunos.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 from random import shuffle
 
+#programa principal
 cabecalho('SORTEIO DE LISTA DE APRESENTAÇÃO')
-
+#1º laço fazendo o programa rodar até que o usuário decida parar
 while True:
-    aluno1 = input('Primeiro aluno: ')
-    aluno2 = input('Segundo aluno: ')
-    aluno3 = input('Terceiro aluno: ')
-    aluno4 = input ('Quarto aluno: ')
+    #upper: joga a string para maiúsculo
+    #strip: remove os espaços no começo e no fim
+    aluno1 = input('Primeiro aluno: ').strip().upper()
+    aluno2 = input('Segundo aluno: ').strip().upper()
+    aluno3 = input('Terceiro aluno: ').strip().upper()
+    aluno4 = input ('Quarto aluno: ').strip().upper()
     lista = [aluno1, aluno2, aluno3, aluno4]
     shuffle(lista)
-    print('A ordem de apresentação será:')
-    print(lista)
+    print(f'A ordem de apresentação será: {lista}')
     print()
     resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
     while resposta not in 'SN':
+        #[0] captura apenas o primeiro caractere
         resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if resposta == 'N':
-        break
     print()
-
+    if resposta == 'N':
+         #quebrando o 1º laço
+        break
 rodape()

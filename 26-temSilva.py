866ecf10adb6291161f8b6d0065f5bab58fa1eba
Danielle Farias <danielle8farias@gmail.com>
2019-06-12 17:34:03 -0300

    adicionando comentários

--- 26-temSilva.py
+++ 26-temSilva.py
@@ -1,5 +1,27 @@
 '''
 Leia uma string e procure pela palavra Silva nela.
 '''
-nome = input('Qual seu nome completo? ').strip()
-print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
+#importando parte do código
+from mensagem import cabecalho
+from mensagem import rodape
+
+#programa principal
+cabecalho('tem silva?')
+#1º laço fazendo o programa rodar até que o usuário decida parar
+while True:
+    #strip: remove os espaços no começo e no fim
+    #lower: joga a string para maiúsculo
+    nome = input('Qual seu nome completo? ').strip().lower()
+    print(f'Seu nome tem Silva? {"silva" in nome}')
+    print()
+    resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
+    while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #[0] captura apenas o primeiro caractere
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    print()
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
+rodape()

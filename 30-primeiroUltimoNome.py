f128d2d2099fb084e4a17e0f0fcae43e69e1e252
Danielle Farias <danielle8farias@gmail.com>
2019-06-13 15:13:09 -0300

    adicionando comentários

--- 30-primeiroUltimoNome.py
+++ 30-primeiroUltimoNome.py
@@ -1,7 +1,31 @@
 '''
 Leia o nome completo e retorne apenas o primeiro e o último nome.
 '''
-nome = input('Digite seu nome completo: ').strip()
-n = nome.split()
-print('Seu primeiro nome é: {}'.format(n[0]))
-print('Seu último nome é: {}'.format(n[-1]))
+#importando parte do código
+from mensagem import cabecalho
+from mensagem import rodape
+
+#programa principal
+cabecalho('Retornando o primeiro e último nome')
+#1º laço fazendo o programa rodar até que o usuário decida parar
+while True:
+    #strip() remove os espaços no começo e no fim
+    nome = input('Digite seu nome completo: ').strip()
+    #split() transforma a string em lista
+    n = nome.split()
+    #[0] pegando o primeiro item da lista
+    print(f'Seu primeiro nome é: {n[0]}')
+    #[-1] pegando o último item da lista
+    print(f'Seu último nome é: {n[-1]}')
+    print()
+    resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
+    while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #[0] captura apenas o primeiro caractere
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    print()
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
+rodape()

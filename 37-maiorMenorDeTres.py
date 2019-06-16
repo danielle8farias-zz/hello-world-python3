881314e17b041b569d3ad46618791d12c184e617
Danielle Farias <danielle8farias@gmail.com>
2019-06-16 16:19:35 -0300

    adicionando comentários

--- 37-maiorMenorDeTres.py
+++ 37-maiorMenorDeTres.py
@@ -1,33 +1,43 @@
 '''
 Leia 3 números e mostre o maior e o menor entre eles.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
-cabecalho('MAIOR ENTRE TRÊS NÚMEROS')
-
-while True:
-    num1 = int(input("Digite o 1º número: "))
-    num2 = int(input("Digite o 2º número: "))
-    num3 = int(input("Digite o 3º número: "))
+#função que verifica o maior número
+def f_maior(num1, num2, num3):
     if num1 >= num2 >= num3:
-        print("O maior número é",num1,"e o menor é",num3)
+        print(f'O maior número é {num1} e o menor é {num3}')
     elif num1 >= num3 >= num2:
-        print("O maior número é",num1,"e o menor é",num2)
+        print(f'O maior número é {num1} e o menor é {num2}')
     elif num2 >= num1 >= num3:
-        print("O maior número é",num2,"e o menor é",num3)
+        print(f'O maior número é {num2} e o menor é {num3}')
     elif num2 >= num3 >= num1:
-        print("O maior número é",num2,"e o menor é",num1)
+        print(f'O maior número é {num2} e o menor é {num1}')
     elif num3 >= num1 >= num2:
-        print("O maior número é",num3,"e o menor é",num2)
+        print(f'O maior número é {num3} e o menor é {num2}')
     else:
-        print("O maior número é",num3,"e o menor é",num1)
+        print(f'O maior número é {num3} e o menor é {num1}')
+
+#programa principal
+cabecalho('MAIOR ENTRE TRÊS NÚMEROS')
+#1º laço fazendo o programa rodar até que o usuário decida parar
+while True:
+    num1 = int(input("Digite o 1º número inteiro: "))
+    num2 = int(input("Digite o 2º número inteiro: "))
+    num3 = int(input("Digite o 3º número inteiro: "))
+    f_maior(num1, num2, num3)
     print()
     resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
     while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
         resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if resposta == 'N':
-        break
     print()
-
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
 rodape()

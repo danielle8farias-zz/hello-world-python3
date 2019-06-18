eb9b627422c3f63b13d2038a5af322691c198df3
Danielle Farias <danielle8farias@gmail.com>
2019-06-18 13:41:17 -0300

    adicionando comentários e funções

--- 39-emprestimoBancario.py
+++ 39-emprestimoBancario.py
@@ -3,27 +3,38 @@ Escreva um programa para aprovação de empréstimo bancário para compra de uma
 casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele
 vai pagar. A prestação será negada se exceder 30% do salário do comprador.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
-cabecalho('APROVAÇÃO DE EMPRÉSTIMO')
-
-while True:
-    valor_casa = float(input('Valor da casa que deseja comprar: R$'))
-    salario = float(input('Salário do comprador: R$'))
-    anos = int(input('Em quantos anos pretende pagar? '))
+#função que calcula a prestação mensal e se o empréstimo foi aprovado
+def prestacao(valor_casa, anos, salario):
     prestacao_mensal = valor_casa / (anos * 12)
-    print('O valor da prestação mensal será de R${:.2f}'.format(prestacao_mensal))
+    #:.2f limita o número de duas casas decimais
+    print(f'O valor da prestação mensal será de R${prestacao_mensal:.2f}')
     if prestacao_mensal < (salario * 0.3):
         print('Empréstimo aprovado!')
     else:
         print('Empréstimo negado.')
+
+#programa principal
+cabecalho('APROVAÇÃO DE EMPRÉSTIMO')
+#1º laço fazendo o programa rodar até que o usuário decida parar
+while True:
+    valor_casa = float(input('Valor da casa que deseja comprar: R$ '))
+    salario = float(input('Salário do comprador: R$ '))
+    anos = int(input('Em quantos anos pretende pagar? '))
+    prestacao(valor_casa, anos, salario)
     print()
     resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
     while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
         resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if resposta == 'N':
-        break
     print()
-
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
 rodape()

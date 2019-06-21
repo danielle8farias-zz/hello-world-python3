6d9e75e639f138804f9b6b8414eede58f67d616f
Danielle Farias <danielle8farias@gmail.com>
2019-06-21 15:43:16 -0300

    adicionando comentários e funções

--- 43-categoriaAtleta.py
+++ 43-categoriaAtleta.py
@@ -7,17 +7,13 @@ até 19 anos = júnior
 até 25 anos = sênior
 acima de 25 = master
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 from datetime import date
 
-cabecalho('CLASSIFICAÇÃO DE ATLETAS')
-
-atual = date.today().year
-while True:
-    nasc = int(input('Digite a data de nascimento: '))
-    idade = atual - nasc
-    print('Sua idade é {} anos.'.format(idade))
+#função que faz a classificação do atleta
+def classificacao(idade):
     if idade < 10:
         print('Classificação: Mirim')
     elif idade < 15:
@@ -28,12 +24,31 @@ while True:
         print('Classificação: Sênior')
     else:
         print('Classificação: Master')
+
+#programa principal
+cabecalho('CLASSIFICAÇÃO DE ATLETAS')
+#date.today().year pega o ano do SO
+atual = date.today().year
+#1º laço fazendo o programa rodar até que o usuário decida parar
+while True:
+    nasc = int(input('Digite o ano de nascimento: '))
+    idade = atual - nasc
+    #fazendo a validação da idade
+    if idade <= 0 or idade >= 100:
+        print('Idade inválida! Digite novamente')
+    else:
+        print(f'Sua idade é {idade} anos.')
+        classificacao(idade)
     print()
     resposta = ' '
+    #2º laço enquanto a resposta não for S ou N
     while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
         resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if resposta == 'N':
-        break
     print()
-
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
 rodape()

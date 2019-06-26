1f0fe90a0bb750b403768638a282661f006768c6
Danielle Farias <danielle8farias@gmail.com>
2019-06-26 18:10:59 -0300

    adicionando comentários e funções

--- 51-palindromo.py
+++ 51-palindromo.py
@@ -1,29 +1,46 @@
 '''
 Leia uma frase e verique se é palíndromo.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
-cabecalho('palíndromo')
-
-while True:
-    frase = input('Digite uma frase: ').strip().lower()
-    palavras = frase.split() #divide uma string em uma lista
-    junto = ''.join(palavras) #junta listas strings separada pelo caractere ''
+#função que verifica se é palíndromo
+def f_palindromo(frase):
+    #split() divide uma string em uma lista
+    palavras = frase.split()
+    #junta listas strings separada pelo caractere ''(sem espaço)
+    #retirando os espaços entre as palavras
+    junto = ''.join(palavras)
     inverso = ''
+    #laço que vai do tamanho da palavra-1 até a última string de -1 em -1
+    #começa do tamanho da palavra-1 porque vai até -1
     for letra in range(len(junto)-1, -1, -1):
+        #somando cada string detrás pra frente
         inverso += junto[letra]
     if inverso == junto:
         print('É um palíndromo!')
         print(inverso)
     else:
         print('Não é palíndromo')
-    print()
-    resp = ' '
-    while resp not in 'SN':
-        resp = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if resp == 'N':
-        break
-    print()
 
+#programa principal
+cabecalho('palíndromo')
+#1º laço fazendo o programa rodar até que o usuário decida parar
+while True:
+    #strip: remove os espaços no começo e no fim
+    #lower: joga a string para minúsculo
+    frase = input('Digite uma frase: ').strip().lower()
+    f_palindromo(frase)
+    print()
+    resposta = ' '
+    #laço enquanto a resposta não for S ou N
+    while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #[0] captura apenas o primeiro caractere
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    print()
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
 rodape()

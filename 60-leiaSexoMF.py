bbb8686c8555a48d7f6570977fef855f386a5716
Danielle Farias <danielle8farias@gmail.com>
2019-07-06 12:27:13 -0300

    inserindo comentários

--- 60-leiaSexoMF.py
+++ 60-leiaSexoMF.py
@@ -2,8 +2,19 @@
 Leia o sexo do usuário e só aceite os valores F ou M.
 Caso esteja errado, insista até o valor correto.
 '''
+#importando parte do código
+from mensagem import cabecalho
+from mensagem import rodape
+
+#programa principal
+cabecalho('Sexo do usuário')
+#upper: joga a string para maiúsculo
+#strip: remove os espaços no começo e no fim
+#[0] captura apenas o primeiro caractere
 sexo = input('Digite o sexo do usuário [F/M]: ').strip().upper()[0]
+#laço que só aceita strings F ou M
 while sexo not in 'FM':
     print('Entrada errada. Favor digite novamente.')
     sexo = input('Digite o sexo do usuário [F/M]: ').strip().upper()[0]
-print('Sexo {} registrado com sucesso.'.format(sexo))
+print(f'Sexo {sexo} registrado com sucesso.')
+rodape()

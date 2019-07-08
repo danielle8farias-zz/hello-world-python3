62d56d35badc8667084258abc98e37d74f524f18
Danielle Farias <danielle8farias@gmail.com>
2019-07-08 16:51:42 -0300

    inserindo comentários

--- 62-somaVariosValores.py
+++ 62-somaVariosValores.py
@@ -1,13 +1,20 @@
 '''
-Leia vários números inteiros e ao final apresenta a quantidade de números
+Leia vários números inteiros e ao final apresente a quantidade de números
 digitados e a soma entre eles.
 Parar quando digitar 999.
 '''
+#importando parte do código
+from mensagem import cabecalho
+from mensagem import rodape
+
+#programa principal
+cabecalho('soma de vários números')
 n = int(input('Digite um número [999 para parar]: '))
+#inicializando as variáveis soma e cont
 soma = cont = 0
 while n != 999:
     soma += n
-    n = int(input('Digite um número [999 para parar]: '))
     cont += 1
-print('Foram digitados {} números.'.format(cont))
-print('A soma é {}.'.format(soma))
+    n = int(input('Digite um número [999 para parar]: '))
+print(f'Foram digitados {cont} números.')
+print(f'A soma é {soma}.')

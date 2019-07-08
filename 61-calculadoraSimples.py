daaba0e56bb6a126bcc3b491beeb3d5d9449a0ea
Danielle Farias <danielle8farias@gmail.com>
2019-07-07 21:06:51 -0300

    inserindo comentários

--- 61-calculadoraSimples.py
+++ 61-calculadoraSimples.py
@@ -1,11 +1,12 @@
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 from mensagem import linha
 
+#programa principal
 cabecalho('calculadora simples')
-
-num1 = int(input('Digite o 1º número: '))
-num2 = int(input('Digite o 2º número: '))
+num1 = int(input('Digite o 1º número inteiro: '))
+num2 = int(input('Digite o 2º número inteiro: '))
 linha()
 print('''
 [1] somar
@@ -15,19 +16,20 @@ print('''
 [5] sair
 ''')
 opcao = int(input('Escolha uma das opções: '))
+#inicializando o resultado
 resultado = 0
 while opcao != 5:
     if opcao == 1:
         resultado = num1 + num2
-        print('A soma é: {}'.format(resultado))
+        print(f'A soma é: {resultado}')
     elif opcao == 2:
         resultado = num1 * num2
-        print('A multiplicação é: {}'.format(resultado))
+        print(f'A multiplicação é: {resultado}')
     elif opcao == 3:
         if num1 > num2:
-            print('O maior é {}'.format(num1))
+            print(f'O maior é {num1}')
         else:
-            print('O maior é {}'.format(num2))
+            print(f'O maior é {num2}')
     elif opcao == 4:
         print('Quais são os números novamente:')
         num1 = int(input('Digite o 1º número: '))
@@ -43,5 +45,4 @@ while opcao != 5:
     [5] sair
     ''')
     opcao = int(input('Escolha uma das opções: '))
-
 rodape()

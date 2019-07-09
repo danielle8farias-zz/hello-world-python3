0116266e46608815ac27689ee121a6c4b05e3d03
Danielle Farias <danielle8farias@gmail.com>
2019-07-09 17:42:16 -0300

    inserindo comentários

--- 64-notaCompraLoja.py
+++ 64-notaCompraLoja.py
@@ -5,15 +5,20 @@ Total de gasto da compra;
 Quantos produtos custam mais de R$ 1000.00;
 O nome do produto mais barato.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 from mensagem import linha
 
+#programa principal
 cabecalho('LOJA SUPER BARATÃO')
-
+#inicializando variáveis
 total = cont = quant = menor = 0
 produto_barato = ' '
+#1º laço fazendo o programa rodar até que o usuário decida parar
 while True:
+    #upper: joga a string para maiúsculo
+    #strip: remove os espaços no começo e no fim
     nome = input('Nome do produto: ').upper().strip()
     preco = float(input('Preço: R$ '))
     quant += 1
@@ -25,13 +30,14 @@ while True:
         produto_barato = nome
     pergunta = ' '
     while pergunta not in 'SN':
+        #[0] captura apenas o primeiro caractere
         pergunta = input('Quer continuar? [S/N] ').upper().strip()[0]
     if pergunta == 'N':
+        #quebrando o 1º laço
         break
 print()
 linha()
-print('Total da compra: R$ {:.2f}'.format(total))
-print('Temos {} produtos custando mais de R$ 1000.00'.format(cont))
-print('O produto mais barato foi {}; custando R$ {:.2f}'.format(produto_barato, menor))
-
+print(f'Total da compra: R$ {total:.2f}')
+print(f'Temos {cont} produtos custando mais de R$ 1000.00')
+print(f'O produto mais barato foi {produto_barato}; custando R$ {menor:.2f}')
 rodape()

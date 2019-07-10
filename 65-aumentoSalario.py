a67576bc258ac6e167050347a236f154147f53e3
Danielle Farias <danielle8farias@gmail.com>
2019-07-10 15:36:26 -0300

    inserindo comentários

--- 65-aumentoSalario.py
+++ 65-aumentoSalario.py
@@ -1,22 +1,28 @@
 '''Calcula o aumento de salário dado o valor e a porcentagem'''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
+#programa principal
 cabecalho('AUMENTO DE SALÁRIO')
-
+#1º laço fazendo o programa rodar até que o usuário decida parar
 while True:
     salario = float(input('Salário: R$ '))
     porcentagem = float(input('Porcentagem de aumento: '))
     aumento = salario * porcentagem/100
     salario = salario + aumento
-    print('O aumento foi de R$ {:.2f}'.format(aumento))
-    print('O novo salário é R$ {:.2f}'.format(salario))
+    print(f'O aumento foi de R$ {aumento:.2f}')
+    print(f'O novo salário é R$ {salario:.2f}')
     print()
     resposta = ' '
+    #laço enquanto a resposta não for S ou N
     while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
         resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
-    if resposta == 'N':
-        break
     print()
-
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
 rodape()

9668c1d6aed522b7d5372f0b88525128c14c9d97
Danielle Farias <danielle8farias@gmail.com>
2019-07-21 21:47:23 -0300

    inserindo comentários

--- 70-maiorMenorTupla.py
+++ 70-maiorMenorTupla.py
@@ -2,17 +2,31 @@
 Crie cinco números aleatórios e armazenar numa tupla. Mostrar os números gerados
 e indicar o menor e o maior deles.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
-
-cabecalho('MAIOR E MENOR EM TUPLA')
-
 from random import randint
-n = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10),
-    randint(1, 10))
-print('Sorteei os valores {}'.format(n))
-print()
-print('O maior valor é: {}'.format(max(n)))
-print('O menor valor é: {}'.format(min(n)))
 
+#programa principal
+cabecalho('MAIOR E MENOR EM TUPLA')
+#1º laço fazendo o programa rodar até que o usuário decida parar
+while True:
+    #criando tuplas de 5 números aleatórios de 1 a 10
+    n = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+    print(f'Sorteei os valores {n}')
+    print()
+    print(f'O maior valor é: {max(n)}')
+    print(f'O menor valor é: {min(n)}')
+    print()
+    resposta = ' '
+    #laço enquanto a resposta não for S ou N
+    while resposta not in 'SN':
+        #upper: joga a string para maiúsculo
+        #strip: remove os espaços no começo e no fim
+        #[0] captura apenas o primeiro caractere
+        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    print()
+    if resposta == 'N':
+        #quebrando o 1º laço
+        break
 rodape()

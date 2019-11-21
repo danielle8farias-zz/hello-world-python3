585aa3bb4bcb9b9103362290e8e8507f31a5b545
Danielle Farias <danielle8farias@gmail.com>
2019-11-21 10:35:42 -0300

    programa cria uma lista de tabela de preços através de uma tupla.

--- ex73_precoComTupla.py
+++ ex73_precoComTupla.py
@@ -2,11 +2,13 @@
 Crie um programa com apenas uma tupla, com nomes de produtos e seus respectivos
 preços.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
+#programa principal
 cabecalho('TABELA DE PREÇOS')
-
+#tupla
 lista = ('lápis', 1.25,
         'borracha', 0.5,
         'caderno', 12.90,
@@ -15,9 +17,13 @@ lista = ('lápis', 1.25,
         'caneta', 2.99,
         'régua', 0.99)
 for i in range(0, len(lista)):
+    #pegando os itens da tupla de índice par: os nomes
     if i % 2 == 0:
+        #.<30 trinta pontos alinhados à esquerda
         print(f'{lista[i]:.<30}', end='')
+    #pegando os itens da tupla de índice impar: os preços
     else:
-        print(f'R$ {lista[i]:>4.2f}')
-
+        #>4 alinhado à direita com 4 posições
+        #.2f com duas casas decimais
+        print(f'R$ {lista[i]:>5.2f}')
 rodape()

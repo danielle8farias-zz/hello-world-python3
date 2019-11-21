97c266d20bd4d1efb6f20f36d6b3152e0e6ba334
Danielle Farias <danielle8farias@gmail.com>
2019-11-21 10:38:00 -0300

    Numa tupla de palavras, programa retorna as vogais.

--- ex74_contandoVogaisEmTupla.py
+++ ex74_contandoVogaisEmTupla.py
@@ -2,16 +2,23 @@
 Crie um programa que tenha em uma tupla, várias palavras (Não usar acentos).
 Retornar para cada palavra as vogais contidas.
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
+#programa principal
 cabecalho('CONTAGEM DE VOGAIS EM UMA TUPLA')
-
+#tupla
 palavras = ('aprender', 'programar', 'linguagem','python','curso','gratis',
             'estudar', 'praticar','trabalhar','mercado','programador','futuro')
+#laço para percorrer a tupla
 for p in palavras:
+    #\n quebra a linha
+    #upper() coloca as letras em maiúsculo
     print(f'\n Na palavra {p.upper()} temos', end=' ')
+    #laço para percorrer a string
     for letra in p:
+        #verifica se na string há alguma vogal
         if letra in 'aeiou':
             print(letra, end=' ')
 print()

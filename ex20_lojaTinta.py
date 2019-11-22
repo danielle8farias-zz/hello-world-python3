f910a2c956847651b3dc336ea32d6ac61ad77681
Danielle Farias <danielle8farias@gmail.com>
2019-11-22 11:56:11 -0300

    loja de tintas onde o usuário informa a área a ser pintada e o programa retorna a quantidade em litros a ser usada e os preços das latas e galões disponíveis

--- ex20_lojaTinta.py
+++ ex20_lojaTinta.py
@@ -24,51 +24,47 @@ from math import ceil
 #programa principal
 cabecalho('loja de tintas')
 area = float(input('Tamanho da área (m²): '))
+#redimento em litros
 rendimento = area/6
-print(f'Será necessário {rendimento:.2f} litros de tinta.')
-
-#cálculo para latas de 18 litros
-# 0.9 para deixar a folga de 10% em cada lata
-folga = 18 * 0.9 #16.2 litros
-lata = ceil(rendimento//folga)
-sobra_lata = (lata*18)-rendimento
-if sobra_lata < 0:
-    sobra_lata *= -1
-preco_lata = lata * 80
+#{:.2f} para duas casas decimais
+print(f'Serão necessários {rendimento:.2f} litros de tinta.')
+print()
 linha()
-print('Para latas de 18 litros cada:')
-if rendimento < 18 or lata <= 1:
+print('-- Para latas de 18 litros cada:')
+# 0.9 para deixar a folga de 10% em cada lata
+# total de litros na lata real 16.2
+folga = 18 * 0.9
+print(f'Cada lata cabe = {folga} litros. Com folga de 10%.')
+# divisão inteira pois não existe 1/2 lata
+lata = ceil(rendimento//folga)
+sobra_lata = rendimento%folga
+preco_lata = lata * 80
+print()
+if rendimento <= folga:
     print('Total de latas: 1')
     print('Valor: R$ 80,00')
     print('Não há sobras.')
-elif sobra_lata < 18 or lata > 1:
-    lata += 1
-    preco_lata += 80
+else:
     print(f'Total de latas: {lata}')
     print(f'Valor: R$ {preco_lata:.2f}')
     print(f'Houve uma sobra de: {sobra_lata:.2f} litros')
-
-#cálculo para galões de 4 litros
-folga = 4 * 0.9 #16.2 litros
+print()
+print('-- Para galões de 4 litros cada:')
+# 0.9 para deixar a folga de 10% em cada lata
+# total de litros no galão real 3.6
+folga = 4 * 0.9
+print(f'Cada galão cabe = {folga} litros. Com folga de 10%.')
+# divisão inteira pois não existe 1/2 galao
 galao = ceil(rendimento//folga)
-sobra_galao = (galao * 4)-rendimento
-if sobra_galao < 0:
-    sobra_galao *= -1
+sobra_galao = rendimento%folga
 preco_galao = galao * 25
-linha()
-print('Para galões de 4 litros cada:')
-if rendimento < 4 or galao <= 1:
+print()
+if rendimento <= folga:
     print('Total de galões: 1')
     print('Valor: R$ 25,00')
     print('Não há sobras.')
-elif sobra_galao < 4 or galao > 1:
-    galao += 1
-    preco_galao += 25
+else:
     print(f'Total de galões: {galao}')
     print(f'Valor: R$ {preco_galao:.2f}')
     print(f'Houve uma sobra de: {sobra_galao:.2f} litros')
-
-#incompleto
-#misturando latas e galões de forma que o preço seja menor
-
 rodape()

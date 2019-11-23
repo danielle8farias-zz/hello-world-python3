1b849c71dcdb5c7a8ec874b75fd850efa262e4d5
Danielle Farias <danielle8farias@gmail.com>
2019-11-23 16:50:44 -0300

    usuário entra com uma palavra e programa retorna a string com as vogais trocadas pelo caractere *

--- ex78_palavraTrocaVogal.py
+++ ex78_palavraTrocaVogal.py
@@ -1,26 +1,38 @@
 '''
 Faça um programa que leia uma palavra e troque as vogais por "*" (asterísco).
 '''
+#importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape
 
+#programa principal
 cabecalho('TROCA VOGAIS POR ASTERÍSCO')
-
+#1º laço fazendo o programa rodar até que o usuário decida parar
 while True:
-   palavra = input('Escreva uma palavra: ').strip().lower()
-   i = 0
-   troca = ''
-   while i < len(palavra):
-      if palavra[i] in 'aeiou':
-         troca += '*'
-      else:
-         troca += palavra[i]
-      i += 1
-   print(f'A nova palavra é: {troca}')
-   resp = ' '
-   while resp not in 'SN':
-      resp = input('Deseja continuar? [S/N] ').strip().upper()[0]
-   if resp == 'N':
-      break
-
+    #strip: remove os espaços no começo e no fim
+    #lower joga a string para minúsculo
+    palavra = input('Escreva uma palavra: ').strip().lower()
+    i = 0
+    #inicializando a variável do tipo string
+    troca = ''
+    while i < len(palavra):
+        if palavra[i] in 'aeiou':
+            #troca = troca + '*'
+            troca += '*'
+        else:
+            #troca = troca + palavra[i]
+            #recebe a letra diferente de vogal
+            troca += palavra[i]
+        i += 1
+    print(f'A nova palavra é: {troca}')
+    #inicializando a variável com caractere vazio para que ela permaneça no loop
+    resp = ' '
+    #laço enquanto a resposta não for S ou N
+    while resp not in 'SN':
+        #upper: joga a string para maiúsculo
+        #[0] captura apenas o primeiro caractere
+        resp = input('Deseja continuar? [S/N] ').strip().upper()[0]
+    if resp == 'N':
+        break
+    print()
 rodape()

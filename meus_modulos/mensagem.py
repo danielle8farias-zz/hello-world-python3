1ec091984742ab4d863287bbf4eccfcb53211ab8
Danielle <danielle8farias@gmail.com>
2020-03-07 20:21:05 -0300

    separando a pasta de módulos criados por mim

---
+++ meus_modulos/mensagem.py
@@ -0,0 +1,15 @@
+def cabecalho(msg):
+    print('-'*50)
+    print(f'{msg.upper():^50}')
+    print('-'*50)
+
+
+def rodape():
+    print('-'*50)
+    print(f'{str.upper("fim"):^50}')
+    print('-'*50)
+
+
+def linha():
+    print('='*50)
+

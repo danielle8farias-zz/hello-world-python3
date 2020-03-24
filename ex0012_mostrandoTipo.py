b6b09ef0198cd2858bf66793a6f5e8d2aa5bdf0a
Danielle <danielle8farias@gmail.com>
2020-03-24 17:49:47 -0300

    adicionando caminho dos meus módulos

--- ex0012_mostrandoTipo.py
+++ ex0012_mostrandoTipo.py
@@ -2,6 +2,10 @@
 '''
 Digite algo e mostre a tipagem da mesma.
 '''
+
+#adicionando minha pasta de módulos
+import sys
+sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho, rodape
 

2e415674c13ef1a5bea9e5bcae3678db57751905
Danielle <danielle8farias@gmail.com>
2020-03-24 18:47:05 -0300

    adicionando caminho dos meus módulos

--- ex0016_descontoCinco.py
+++ ex0016_descontoCinco.py
@@ -2,6 +2,10 @@
 '''
 Leia o valor e desconte 5%.
 '''
+
+#adicionando minha pasta de módulos
+import sys
+sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho, rodape
 

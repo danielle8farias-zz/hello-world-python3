20d98c26a186a3a8452d30155c5375b44cf80f74
Danielle <danielle8farias@gmail.com>
2020-03-24 18:48:56 -0300

    adicionando caminho dos meus módulos

--- ex0019_porcaoInteira.py
+++ ex0019_porcaoInteira.py
@@ -3,6 +3,10 @@
 Leia um número real qualquer e retorne apenas a parte inteira,
 sem arredondamento.
 '''
+
+#adicionando minha pasta de módulos
+import sys
+sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho, rodape
 from math import trunc

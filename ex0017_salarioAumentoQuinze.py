16a06cbeb02c751d34684b2d99ade2bba4014ab6
Danielle <danielle8farias@gmail.com>
2020-03-24 18:47:38 -0300

    adicionando caminho dos meus módulos

--- ex0017_salarioAumentoQuinze.py
+++ ex0017_salarioAumentoQuinze.py
@@ -2,6 +2,10 @@
 '''
 Leia o salário e o aumente em 15%.
 '''
+
+#adicionando minha pasta de módulos
+import sys
+sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho, rodape
 

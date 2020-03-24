2ca4709eae1691ac23f6801a065c3dea7199855e
Danielle <danielle8farias@gmail.com>
2020-03-24 18:46:04 -0300

    adicionando caminho dos meus módulos

--- ex0014_conversorDolar.py
+++ ex0014_conversorDolar.py
@@ -2,6 +2,10 @@
 '''
 Faça um conversor de Real em Dolar.
 '''
+
+#adicionando minha pasta de módulos
+import sys
+sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho, rodape
 

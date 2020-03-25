96271ae495af55668fd34b91fa123bc8209b39bf
Danielle <danielle8farias@gmail.com>
2020-03-25 15:45:56 -0300

    adicionando caminho para a pasta de módulos

--- ex31_primeiroUltimoNome.py
+++ ex31_primeiroUltimoNome.py
@@ -2,6 +2,10 @@
 '''
 Leia o nome completo e retorne apenas o primeiro e o último nome.
 '''
+
+#adicionando minha pasta de módulos
+import sys
+sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho
 from mensagem import rodape

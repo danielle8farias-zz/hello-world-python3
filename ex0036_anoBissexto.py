621fcf1e1b951466f053c79048f7d400f4caa8e2
Danielle <danielle8farias@gmail.com>
2020-04-03 19:12:51 -0300

    adicionando caminho de módulos criados

--- ex0036_anoBissexto.py
+++ ex0036_anoBissexto.py
@@ -3,6 +3,10 @@
 '''
 Leia um ano qualquer e verifique se é bissexto.
 '''
+
+#adicionando minha pasta de módulos
+import sys
+sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho, rodape
 

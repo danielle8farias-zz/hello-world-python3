c30257fdf62f6b1ac75dc95775cee02d527d3a90
Danielle <danielle8farias@gmail.com>
2020-04-08 13:23:02 -0300

    correção do caminho dos módulos criados

--- ex0014_conversorDolar.py
+++ ex0014_conversorDolar.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3.8
+
 '''
 Faça um conversor de Real em Dolar.
 '''
 
 #adicionando minha pasta de módulos
 import sys
-sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
+sys.path.append('/home/danielle8farias/exercicios-python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho, rodape
 

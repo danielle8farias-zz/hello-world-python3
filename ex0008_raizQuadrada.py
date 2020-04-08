b16cab2d09e511cd3c47db34f71c8a1ab6e9d8cf
Danielle <danielle8farias@gmail.com>
2020-04-08 13:14:00 -0300

    correção do caminho dos módulos criados

--- ex0008_raizQuadrada.py
+++ ex0008_raizQuadrada.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3.8
+
 '''
 Informe a raiz quadrada de um número inteiro.
 '''
 
 #adicionando minha pasta de módulos
 import sys
-sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
+sys.path.append('/home/danielle8farias/exercicios-python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho, rodape
 from math import sqrt

568ca28c752c9cde7b6bfce369712f7832c89fee
Danielle <danielle8farias@gmail.com>
2020-04-08 13:40:20 -0300

    correção do caminho dos módulos criados

--- ex0017_salarioAumentoQuinze.py
+++ ex0017_salarioAumentoQuinze.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3.8
+
 '''
 Leia o salário e o aumente em 15%.
 '''
 
 #adicionando minha pasta de módulos
 import sys
-sys.path.append('/home/danielle8farias/Exercicios-Python-3/meus_modulos')
+sys.path.append('/home/danielle8farias/exercicios-python-3/meus_modulos')
 #importando parte do código
 from mensagem import cabecalho, rodape
 

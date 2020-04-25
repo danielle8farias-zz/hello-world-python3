12e89fa7f190891dec908f235a671a1d1743a5ca
Danielle <danielle8farias@gmail.com>
2020-04-25 16:08:08 -0300

    documentando módulo mensagem

--- meus_modulos/mensagem.py
+++ meus_modulos/mensagem.py
@@ -1,15 +1,32 @@
+'''
+funções para cabeçalho dos scripts python
+'''
+
 def cabecalho(msg):
+    '''
+    recebe uma string e retorna a mesma em caixa alta com espaçamento de 50 caracteres, incluindo a string passada
+    linha pontilhada em baixo e em cima
+    '''
     print('-'*50)
     print(f'{msg.upper():^50}')
     print('-'*50)
 
 
 def rodape():
+    '''
+    sem parâmetro
+    retorna a string FIM com espaçamento de 50 caracteres, incluindo a string
+    linha pontilhada em baixo e em cima
+    '''
     print('-'*50)
     print(f'{str.upper("fim"):^50}')
     print('-'*50)
 
 
 def linha():
+    '''
+    sem parâmetro
+    retorna sinais de = repetidos em 50 caracteres
+    '''
     print('='*50)
 

163f8b92ee6ac148713d84402cc6e3b555f376d0
Danielle <danielle8farias@gmail.com>
2020-04-29 18:54:49 -0300

    trocando vogais das palavras

---
+++ exercicio_py/ex0024_trocaVogal.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.8
+
+'''
+Faça um programa que leia uma palavra e troque as vogais por "*" (asterísco).
+Exercício 77 com utilizando o "for".
+'''
+from mensagem import cabecalho
+from mensagem import rodape
+
+cabecalho('TROCA VOGAIS POR ASTERÍSCO')
+
+palavra = input('Escreva uma palavra: ').strip().lower()
+troca = ''
+for vogal in range(len(palavra)):
+    if palavra[vogal] in 'aeiou':
+        troca += '*'
+    else:
+        troca += palavra[vogal]
+print(f'A nova palavra é: {troca}')
+
+rodape()

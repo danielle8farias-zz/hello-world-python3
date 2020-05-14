09c7bc8354b223a3f9a77dbc733fed535f7b1048
Danielle <danielle8farias@gmail.com>
2020-05-14 19:39:49 -0300

    coisa linda da mãe <3

--- meus_modulos/mensagem.py
+++ meus_modulos/mensagem.py
@@ -1,11 +1,15 @@
 '''
-funções para cabeçalho dos scripts python
+funções para:
+cabeçalho;
+linha;
+rodapé;
+resposta se quer continuar.
 '''
 
-def cabecalho(msg):
+def ler_cabecalho(msg):
     '''
-    recebe uma string e retorna a mesma em caixa alta com espaçamento de 50 caracteres, incluindo a string passada
-    linha pontilhada em baixo e em cima
+    recebe uma string e retorna a mesma em caixa alta com espaçamento de 50 caracteres, incluindo a string passada.
+    linha pontilhada em baixo e em cima.
     '''
     print('-'*50)
     print(f'{msg.upper():^50}')
@@ -15,8 +19,8 @@ def cabecalho(msg):
 def rodape():
     '''
     sem parâmetro
-    retorna a string FIM com espaçamento de 50 caracteres, incluindo a string
-    linha pontilhada em baixo e em cima
+    retorna a string FIM com espaçamento de 50 caracteres, incluindo a string.
+    linha pontilhada em baixo e em cima.
     '''
     print('-'*50)
     print(f'{str.upper("fim"):^50}')
@@ -25,8 +29,42 @@ def rodape():
 
 def linha():
     '''
-    sem parâmetro
-    retorna sinais de = repetidos em 50 caracteres
+    sem parâmetro.
+    retorna sinais de = repetidos em 50 caracteres.
     '''
     print('='*50)
 
+
+def ler_resposta(msg):
+    '''
+    Faz o programa rodar enquanto o usuário digitar S ou SIM
+    '''
+    while True:
+        try:
+            #[0] captura apenas o primeiro caractere
+            #strip() remove os espaços no começo e no fim
+            #upper() transforma em maiúscula
+            #input() captura o que é digitado
+            #atribuindo valor à variável 'resposta'
+            resposta = input(msg).upper().strip()[0]
+            #verifica se não está contido na string 'SN';
+            #   não há conflito, pois 'resposta' captura apenas a posição inicial da string
+            if resposta not in 'SN':
+                #criando exceção
+                raise Exception('S para sim ou N para não')
+        #chama a exceção criada
+        #variável 'e' retorna a mensagem da exceção
+        except Exception as e:
+            #print(f'') retorna uma string formatada na tela
+            print(f'Resposta inválida: {e}')
+            #volta para o início do laço
+            continue
+        #em caso de resposta vazia; 
+        #   caso não haja string para pegar a posição inicial
+        except IndexError:
+            print(f'Resposta inválida.')
+            continue
+        #se o 'try' for válido
+        else:
+            #retorna o valor da resposta
+            return resposta

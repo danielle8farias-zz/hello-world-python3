a13437857aa91759a062fb540a047154823eeabf
Danielle <danielle8farias@gmail.com>
2020-05-15 20:33:33 -0300

    importando módulo de validação de número

--- exercicio_py/ex0003_soma_dois.py
+++ exercicio_py/ex0003_soma_dois.py
@@ -10,27 +10,8 @@ import sys
 sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
 #importando módulos de auxílio
 from mensagem import ler_cabecalho, rodape, linha, ler_resposta
-
-#função que captura um número informado pelo usuário para validação
-def ler_num(n):
-    #laço
-    while True:
-        try:
-            #input() captura como string o que for digitado
-            #float() convertendo a string recebida para tipo flutuante
-            #atribuindo valor à variável 'num'
-            num = float(input(n))
-        #criando exceção
-        #   caso o valor digitado seja diferente de um número
-        except ValueError as erro:
-            #print() retorna uma string na tela
-            print('Digite um valor válido!')
-            #volta para o início do laço
-            continue
-        #se o 'try' for válido        
-        else:
-            #retorna valor de mes
-            return num
+#importando módulos de números
+from numeros import ler_num_float
 
 #função que realiza a operação soma de dois parâmetros
 def somar(n1, n2):
@@ -41,10 +22,10 @@ def somar(n1, n2):
 #chamada da função que lê o cabeçalho
 ler_cabecalho('somar dois números')
 while True:
-    #chamada da função que lê o número
+    #chamada da função que lê números float
     #atribui a variável 'num1' o retorno da função
-    num1 = ler_num('Digite o 1º número: ')
-    num2 = ler_num('Digite o 2º número: ')
+    num1 = ler_num_float('Digite o 1º número: ')
+    num2 = ler_num_float('Digite o 2º número: ')
     soma = somar(num1, num2)
     #print(f'') retorna uma string formatada na tela
     print(f'Soma = {soma}')

f85878ce546ff256ca1c6fee3ca568b8034b1b0c
Danielle <danielle8farias@gmail.com>
2020-05-16 14:45:22 -0300

    adicionando módulos

--- exercicio_py/ex0012_mostrandoTipo.py
+++ exercicio_py/ex0012_mostrandoTipo.py
@@ -16,12 +16,12 @@ import sys
 #adicionando ao final da lista de módulos o caminho para os meus módulos
 sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
 #importando módulo de cabeçalho e rodapé
-from mensagem import cabecalho, rodape
+from mensagem import ler_cabecalho, rodape, ler_resposta, linha
 
 #programa principal
-#chamada da função cabeçalho
-cabecalho('TIPO DE DADO')
-#1º laço fazendo o programa rodar até que o usuário decida parar
+#chamada que lê a função cabeçalho
+ler_cabecalho('mostrando tipos')
+#laço
 while True:
     #atribuindo valor a variável a
     #input() recebe como string dado digitado
@@ -35,22 +35,20 @@ while True:
     print(f'É alfanumérico? {a.isalnum()}')
     print(f'Está em maiúsculas? {a.isupper()}')
     print(f'Está em minúsculas? {a.islower()}')
-    #funciona apenas para string sem números
+    #funciona apenas para string sem números e sem espaços
     print(f'Está capitalizada? {a.istitle()}')
     #função print vazia não retorna nada; pula uma linha
     print()
-    #inicializa a variável com espaço para entrar no 2º laço
-    resposta = ' '
-    #2º laço enquanto a resposta não for S ou N
-    while resposta not in 'SN':
-        #upper: joga a string para maiúsculo
-        #strip: remove os espaços no começo e no fim
-        #[0] captura apenas o primeiro caractere
-        resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    #chamada da função que lê a resposta
+    resposta = ler_resposta('Deseja continuar? [S/N]')
     print()
-    #verificando se variável reposta é igual a string N
+    #verificando se variável 'reposta' é igual a string N
     if resposta == 'N':
-        #quebrando o 1º laço
+        #quebrando o laço
         break
+    else:
+        #chamada da função linha
+        linha()
+        print()
 #chamada da função rodapé
 rodape()

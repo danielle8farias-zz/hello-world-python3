01452528d1429dc171e3b47dc65eff55ace1e92c
Danielle <danielle8farias@gmail.com>
2020-05-19 12:52:18 -0300

    método de classe

--- exercicio_py/ex0048_classe_empregado.py
+++ exercicio_py/ex0048_classe_empregado.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python3.8
+
+#importando o módulo de informações sobre o sistema
+import sys
+#adicionando ao final da lista de módulos o caminho para os meus módulos
+sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
+#importando módulo de linha
+from mensagem import linha
+
 #criando classe
 class Empregado:
     #variável da classe
-    aumento = 1.05
+    valor_aumento = 1.05
     #variável de instância
     num_empregados = 0
 
@@ -24,11 +33,17 @@ class Empregado:
     def aumentar_salario(self):
         #aumento de 5% do salário
         #'self.aumento' ou 'Empregado.aumento'
-        self.pagamento = int(self.pagamento * self.aumento)
+        self.pagamento = int(self.pagamento * self.valor_aumento)
+
+    @classmethod
+    def definir_aumento(cls, novo_aumento):
+        #usa-se 'cls' ao invés de 'self', pois é um método de classe
+        cls.valor_aumento = novo_aumento
 
 
-print(f'Número de empregados: {Empregado.num_empregados}')
+print(f'Número de empregados inicial: {Empregado.num_empregados}')
 print()
+
 #instanciado objeto
 pessoa1 = Empregado('Enrico', 'Sandro', 8000)
 #imprimindo na tela atributo 'nome'
@@ -37,35 +52,64 @@ print(f'Sobrenome: {pessoa1.sobrenome}')
 print(f'email: {pessoa1.email}')
 print(f'Nome completo: {pessoa1.nome_completo()}')
 print(f'Salário atual: R${pessoa1.pagamento}')
-pessoa1.aumentar_salario()
-print(f'Aumento: R${pessoa1.pagamento}')
-#mapeando atributos do objeto para dicionário
-print(pessoa1.__dict__)
-print()
-print(f'Número de empregados: {pessoa1.num_empregados}')
 print()
 
+pessoa1.aumentar_salario()
+print(f'Aumento: R${pessoa1.pagamento}')
+print()
+
+print('Mapeando atributos do objeto para dicionário:')
+print(pessoa1.__dict__)
+print()
+
+print(f'Número de empregados: {pessoa1.num_empregados}')
+linha()
+
+print('Inserindo novo usuário')
 nome = input('Digite seu nome: ').strip()
 sobrenome = input('Digite seu sobrenome: ').strip()
 salario = float(input('Seu salário? R$'))
-
-pessoa = Empregado(nome, sobrenome, salario)
-print(f'Seu email será: {pessoa.email}')
-print(f'Nome completo: {pessoa.nome_completo()}')
-print(f'Salário atual: R${pessoa.pagamento}')
-pessoa.aumentar_salario()
-print(f'Aumento: R${pessoa.pagamento}')
-print(pessoa.__dict__)
-print()
-print(f'Número de empregados: {pessoa.num_empregados}')
 print()
 
-#acessando a variável da classe
-print(Empregado.aumento)
-pessoa2 = Empregado('Fulano', 'Silva', 2000)
-print(pessoa2.aumento)
+print('Exibindo dados do novo usuário')
+pessoa2 = Empregado(nome, sobrenome, salario)
+print(f'Seu email será: {pessoa2.email}')
+print(f'Nome completo: {pessoa2.nome_completo()}')
+print(f'Salário atual: R${pessoa2.pagamento}')
+print()
+
+pessoa2.aumentar_salario()
+print(f'Aumento: R${pessoa2.pagamento}')
+print()
+
+#mapeando atributos do objeto para dicionário
+print('Mapeando atributos do objeto para dicionário:')
 print(pessoa2.__dict__)
-Empregado.aumento = 2
-print(pessoa2.aumento)
 print()
+
 print(f'Número de empregados: {pessoa2.num_empregados}')
+linha()
+
+pessoa3 = Empregado('Fulano', 'Silva', 2000)
+
+print('acessando a variável da classe')
+print(Empregado.valor_aumento)
+print(pessoa3.valor_aumento)
+print()
+
+print('Atribuindo aumento de 30%')
+Empregado.valor_aumento = 1.3
+print(pessoa3.valor_aumento)
+print()
+
+print(f'Atribuindo novo aumento de 50% através do método de classe')
+Empregado.definir_aumento(1.50)
+print(pessoa3.valor_aumento)
+print()
+
+print('Mapeando atributos do objeto para dicionário:')
+print(pessoa3.__dict__)
+print()
+
+print(f'Número final de empregados: {pessoa2.num_empregados}')
+print()

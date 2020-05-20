5d333f5f7c631442c1927c8c1a455339633563fc
Danielle <danielle8farias@gmail.com>
2020-05-20 16:32:26 -0300

    multiplicação

---
+++ exercicio_py/ex0010_multiplicacao.py
@@ -0,0 +1,11 @@
+'''
+Usuário informa dois números (inteiros ou float) e programa retorna a multiplicação entre eles.
+'''
+
+num1 = float(input('Digite o 1º número: '))
+num2 = float(input('Digite o 2º número: '))
+
+#calculando multiplicação
+multi = num1 * num2
+print(f'Resultado da multiplicação: {multi}')
+print()

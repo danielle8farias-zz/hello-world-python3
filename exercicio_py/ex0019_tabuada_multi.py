82e4c9c5472c2531b1a71949a14e420483087f7b
Danielle <danielle8farias@gmail.com>
2020-05-20 21:42:49 -0300

    tabuada de multiplicação

---
+++ exercicio_py/ex0019_tabuada_multi.py
@@ -0,0 +1,15 @@
+'''
+Usuário digita um número e programa retorna a tabuada de multiplicação desse número.
+'''
+
+num = float(input('Digite um número: '))
+print(f'{num:4} x 1 = {num*1}')
+print(f'{num:4} x 2 = {num*2}')
+print(f'{num:4} x 3 = {num*3}')
+print(f'{num:4} x 4 = {num*4}')
+print(f'{num:4} x 5 = {num*5}')
+print(f'{num:4} x 6 = {num*6}')
+print(f'{num:4} x 7 = {num*7}')
+print(f'{num:4} x 8 = {num*8}')
+print(f'{num:4} x 9 = {num*9}')
+print()

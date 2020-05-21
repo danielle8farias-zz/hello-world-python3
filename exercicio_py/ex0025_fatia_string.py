9e45bdb7e65e1d5ae2142cfb78a9fdc6d084a0bd
Danielle <danielle8farias@gmail.com>
2020-05-21 18:30:41 -0300

    fatiando string

---
+++ exercicio_py/ex0025_fatia_string.py
@@ -0,0 +1,18 @@
+'''
+Fatiando string
+'''
+
+msg = 'a lenda de zelda'
+
+#pegando da posição 2 até 6
+print(msg[2:7])
+#pegando da posição 11 até o final da string
+print(msg[11:])
+#pegando da posição inicial até a 10
+print(msg[:11])
+#do início até o fim da string
+#   mostra a primeira posição e então pula duas posições
+#   axlxnxaxdx xexdx
+#   a l n a d   e d
+print(msg[::2])
+print()

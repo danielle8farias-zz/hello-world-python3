51ebe142ad70bf8f735b8b5e1c006d8ae403cfcf
Danielle <danielle8farias@gmail.com>
2020-05-20 21:18:06 -0300

    prints

---
+++ exercicio_py/ex0017_prints_sem_quebra.py
@@ -0,0 +1,11 @@
+'''
+Exibir dois prints separados sem quebra de linha.
+'''
+
+nome = 'Danielle'
+#end padrão quebra a linha;
+#   preenchido com espaço
+print(nome, end=' ')
+sobrenome = 'Farias'
+print(sobrenome)
+print()
---
+++ exercicio_py/ex0018_prints_com_quebra.py
@@ -0,0 +1,8 @@
+'''
+Exibir um único print em linhas separadas.
+'''
+
+nome = 'danielle'
+#\n quebra a linha dentro do print()
+print(f'Seu nome é:\n{nome}')
+print()

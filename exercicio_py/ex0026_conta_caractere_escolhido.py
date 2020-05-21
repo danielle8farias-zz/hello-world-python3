978509119769c50590a6cc52f80dcec90c61b55e
Danielle <danielle8farias@gmail.com>
2020-05-21 18:53:30 -0300

    mais exemplos

--- exercicio_py/ex0026_conta_caractere_escolhido.py
+++ exercicio_py/ex0026_conta_caractere_escolhido.py
@@ -4,8 +4,18 @@ programa retorna a quantidade delas.
 '''
 
 msg = input('Digite uma palavra ou frase: ')
-escolhido = input('Que letra deseja contar? ')
+escolha = input('Que letra deseja contar? ')
 #count() conta quantas vezes aparece o caractere
-num = msg.count(escolhido)
-print(f'Essa palavra tem a letra {escolhido}: {num} vezes.')
+num = msg.count(escolha)
+print(f'Tem a letra {escolha}: {num} vezes.')
+print()
+
+msg1 = 'Um livro é a prova de que os seres humanos são capazes de fazer magia'
+print(len(msg1))
+#contando a letra 'e'
+num1 = msg1.count('e')
+print(f'Quantas letras "e"? {num1}')
+#da posição 7 a 60; contando a letra escolhida
+num2 = msg1.count(escolha, 7, 60)
+print(f'Tem a letra {escolha}: {num2} vezes.')
 print()

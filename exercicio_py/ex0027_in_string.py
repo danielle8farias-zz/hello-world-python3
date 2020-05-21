81af5d5c42dffd6e087cd4cc6634b40825e49c58
Danielle <danielle8farias@gmail.com>
2020-05-21 19:15:47 -0300

    in string

---
+++ exercicio_py/ex0027_in_string.py
@@ -0,0 +1,8 @@
+'''
+Verifica se uma palavra(string) se encontra em uma frase(string).
+'''
+
+frase = 'A ciência não é só compatível com a espiritualidade; é uma profunda fonte de espiritualidade'
+
+print(f'A palavra "ciência" se encontra na frase? {"ciência" in frase}')
+print()

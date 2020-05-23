1c2deecaa81aee51f2436b777a96dac3b8586747
Danielle <danielle8farias@gmail.com>
2020-05-23 17:47:57 -0300

    laço for

---
+++ exercicio_py/ex0033_laco_for.py
@@ -0,0 +1,11 @@
+'''
+Contador de 0 a 10
+'''
+
+#laço for
+# variável 'i' é automaticamente inicializada em 0
+#   enquanto i for menos do que 10
+#   range(inicio, fim, passo)
+#   fim do range deve ser maior +1 do valor que se quer chegar
+for i in range(11):
+    print(i)

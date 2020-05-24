30acaaf0a6631e84cae25363a9cd42c56a112d00
Danielle <danielle8farias@gmail.com>
2020-05-24 12:03:37 -0300

    progressão aritmética

---
+++ exercicio_py/ex36_PA.py
@@ -0,0 +1,15 @@
+'''
+Usuário informa o 1º termo de uma PA e sua razão. 
+O programa retorna os 10 primeiros termos dessa PA.
+'''
+
+A1 =  int(input('Primeiro termo: '))
+r = int(input('Razão: '))
+
+#fóruma da Progressão aritmética
+#   calculando o último termo
+An = A1 + (10-1)*r
+#laço que vai de A1 até An+r de r em r
+for n in range(A1, An + r, r):
+    print(f'{n}', end = ' -> ')
+print('FIM')

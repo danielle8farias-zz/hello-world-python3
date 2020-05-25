4a7821cf001570550dc7a5581fe482384f2a2051
Danielle <danielle8farias@gmail.com>
2020-05-25 14:26:36 -0300

    laço while

---
+++ exercicio_py/ex0038_laco_while.py
@@ -0,0 +1,14 @@
+'''
+Contador de 0 a 10; com while
+'''
+
+#inicializando variável contadora
+i = 0
+
+#laço while
+while i < 11:
+    print(i)
+    #incrementando a variável contadora
+    #isso é necessário para que o programa não fique rodando eternamente
+    i += 1
+print()

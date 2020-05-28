81b1cea50c775c2047c930aa9e97ee6f551d228c
Danielle <danielle8farias@gmail.com>
2020-05-28 13:03:57 -0300

    lista sem valores repetidos

---
+++ exercicio_py/ex0062_valores_unicos_lista.py
@@ -0,0 +1,26 @@
+'''
+Usuário digita vários valores inteiros. Esses valores são guardados em uma lista. 
+Não é permitido números repetidos.
+'''
+
+lista_num = list()
+
+#adicionado números a lista
+while True:
+    num = int(input('Digite um número: '))
+    
+    if num not in lista_num:
+        lista_num.append(num)
+    else:
+        print('Esse número já foi adicionado a lista.\n')
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = input('\nDeseja continuar? [S/N] ').upper().strip()[0]
+    if resp == 'N':
+        break
+
+print(f'\nSua lista: {lista_num}')
+
+lista_num.sort()
+print(f'Lista ordenada: {lista_num}')

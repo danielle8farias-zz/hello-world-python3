71275ec2105df3dce6edf4b8f3362fcf931bb3a8
Danielle <danielle8farias@gmail.com>
2020-06-04 20:20:58 -0300

    dobrando valores de uma lista usando uma função

---
+++ exercicio_py/ex0077_dobra_valor_lista.py
@@ -0,0 +1,15 @@
+'''
+Dobrando valores de uma lista através de uma função.
+'''
+
+def dobrar(lista):
+    for c, v in enumerate(lista):
+        lista[c] = v *2
+    #não retorna nada
+
+
+valores = [7, 5, 3, 9]
+print(valores)
+dobrar(valores)
+#mesmo sem o retorno da função, a lista foi modificada
+print(valores)

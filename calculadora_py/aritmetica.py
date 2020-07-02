82f97c2090e51543741a2816c2b2b18a79ea5f5b
Danielle <danielle8farias@gmail.com>
2020-07-02 20:34:56 -0300

    cabeçalhos

--- calculadora_py/aritmetica.py
+++ calculadora_py/aritmetica.py
@@ -1,27 +1,32 @@
 import sys
 sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
+from mensagem import ler_cabecalho
 from numeros import ler_num_float, ler_divisor, ler_num_int, ler_indice
 
 
 def somar():
+    ler_cabecalho('soma')
     num1 = ler_num_float('Digite o 1º número: ')
     num2 = ler_num_float('Digite o 2º número: ')
     return num1 + num2
 
 
 def subtrair():
+    ler_cabecalho('subtração')
     num1 = ler_num_float('Digite o 1º número: ')
     num2 = ler_num_float('Digite o 2º número: ')
     return num1 - num2
 
 
 def multiplicar():
+    ler_cabecalho('multiplicação')
     num1 = ler_num_float('Digite o 1º número: ')
     num2 = ler_num_float('Digite o 2º número: ')
     return num1 * num2
 
 
 def dividir():
+    ler_cabecalho('divisão')
     num1 = ler_num_float('Digite o 1º número: ')
     num2 = ler_divisor('Digite o 2º número: ')
     return num1 / num2
@@ -29,12 +34,14 @@ def dividir():
 
 from math import pow
 def exponenciar():
+    ler_cabecalho('exponenciação')
     base = ler_num_float('Digite o número da base: ')
     expo = ler_num_int('Digite o número do expoente: ')
     return pow(base, expo)
 
 
 def radiciar():
+    ler_cabecalho('radiciação')
     indice = ler_indice('Índice da raiz: ')
     radicando = ler_num_float('Radicando: ')
     if indice % 2 == 0 and radicando >= 0:
@@ -46,4 +53,4 @@ def radiciar():
         return raiz * -1
     else:
         return 'Não é possível calcular a raiz dentro dos Reais.'
-    
+

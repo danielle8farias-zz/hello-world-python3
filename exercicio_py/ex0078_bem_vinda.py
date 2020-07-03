b2ef83df5ba77907996e09ab4ddcbdcc856bd2b3
Danielle <danielle8farias@gmail.com>
2020-07-03 19:22:31 -0300

    inserindo função que valida string

--- exercicio_py/ex0078_bem_vinda.py
+++ exercicio_py/ex0078_bem_vinda.py
@@ -6,27 +6,12 @@ Lê o nome de um usuário e retorna o nome com uma mensagem de boas-vindas. Faze
 
 import sys
 sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
-from mensagem import ler_cabecalho, rodape, linha, ler_resposta
-
-def ler_nome(msg):
-    while True:
-        try:
-            nome = input(msg).upper().strip()
-            #isalpha() se possui apenas letras
-            #verificando se nome possui caracteres que não sejam letras
-            if not nome.isalpha():
-                raise Exception('Digite apenas letras.')
-        except Exception as erro:
-            print(f'Valor inválido: {erro}')
-            continue
-        #se o 'try' for válido
-        else:
-            return nome
+from mensagem import ler_cabecalho, rodape, linha, ler_resposta, ler_palavra
 
 
 ler_cabecalho('mensagem de boas-vindas')
 while True:
-    nome = ler_nome('Qual seu nome? ')
+    nome = ler_palavra('Qual seu nome? ')
     #verificando se a string não é vazia
     if nome != None:
         print(f'Bem-vinda, {nome}!')

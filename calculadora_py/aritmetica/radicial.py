23f08d4d77a6507fe79f39f16b191bc04423f0f2
Danielle <danielle8farias@gmail.com>
2020-07-08 13:17:10 -0300

    corrigindo problema precisão

--- calculadora_py/aritmetica/radicial.py
+++ calculadora_py/aritmetica/radicial.py
@@ -8,12 +8,15 @@ def radiciar():
     ler_cabecalho('radiciação')
     indice = ler_indice('Índice da raiz: ')
     radicando = ler_num_float('Radicando: ')
-    if indice % 2 == 0 and radicando >= 0:
-        raiz = radicando ** (1/indice)
-        return raiz
-    elif radicando < 0 and indice % 2 == 1:
+    #retorna negativo se índice ímpar e radicando negativo
+    if radicando < 0 and indice % 2 == 1:
         radicando *= -1
         raiz = radicando ** (1/indice)
-        return raiz * -1
+        return round(raiz * -1, 5)
+    
+    elif indice % 2 == 0 or indice % 2 == 1:
+        raiz = radicando ** (1/indice)
+        return round(raiz, 5)
+    
     else:
         return 'Não é possível calcular a raiz dentro dos Reais.'

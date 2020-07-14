432c91cf5d9dbc560897cddb61e566856c5ed41c
Danielle <danielle8farias@gmail.com>
2020-07-14 10:47:30 -0300

    raizes da equação

--- calculadora_py/equacoes_polinomiais/equacao_2_grau.py
+++ calculadora_py/equacoes_polinomiais/equacao_2_grau.py
@@ -6,17 +6,24 @@ from math import pow, sqrt
 from time import sleep
 
 
-def calcular_raiz_delta(delta):
+def calcular_raizes(a, b, raiz_delta):
+    x1 = (-b + raiz_delta)/(2 * a)
+    x2 = (-b - raiz_delta)/(2 * a)
+    print(f'Raizes: {x1} e {x2}')
+
+
+def calcular_raiz_delta(a, b, delta):
     raiz_delta = sqrt(delta)
     sleep(0.5)
     print(f'Raiz quadrada de Delta: {raiz_delta:.4f}')
+    calcular_raizes(a, b, raiz_delta)
 
 
 def calcular_delta(a, b, c):
     delta = (pow(b, 2)) - 4 * a * c
     sleep(0.5)
     print(f'Delta: {delta}')
-    calcular_raiz_delta(delta)
+    calcular_raiz_delta(a, b, delta)
 
 
 def equacao_seg_grau():

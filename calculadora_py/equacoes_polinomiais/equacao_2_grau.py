fc3d97e93e04eafe9579adae3f4ac114a3238913
Danielle <danielle8farias@gmail.com>
2020-07-14 10:39:37 -0300

    raiz do delta

--- calculadora_py/equacoes_polinomiais/equacao_2_grau.py
+++ calculadora_py/equacoes_polinomiais/equacao_2_grau.py
@@ -2,14 +2,21 @@ import sys
 sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
 from numeros import ler_num_float
 
-from math import pow
+from math import pow, sqrt
 from time import sleep
+
+
+def calcular_raiz_delta(delta):
+    raiz_delta = sqrt(delta)
+    sleep(0.5)
+    print(f'Raiz quadrada de Delta: {raiz_delta:.4f}')
 
 
 def calcular_delta(a, b, c):
     delta = (pow(b, 2)) - 4 * a * c
     sleep(0.5)
     print(f'Delta: {delta}')
+    calcular_raiz_delta(delta)
 
 
 def equacao_seg_grau():

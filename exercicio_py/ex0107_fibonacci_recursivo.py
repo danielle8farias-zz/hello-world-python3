b6a2e389c36a7a3b3fa9f3fdb0758cced3876c59
Danielle <danielle8farias@gmail.com>
2020-07-15 16:58:39 -0300

    fibonacci usando recursão

--- exercicio_py/ex0107_fibonacci_recursivo.py
+++ exercicio_py/ex0107_fibonacci_recursivo.py
@@ -17,7 +17,7 @@ def fibo(n):
 
 ler_cabecalho('fibonacci recursivo')
 num = ler_num_nat('Digite um número: ')
-#num-1 para considerar que a sequência começa em 0
+#num-1 para considerar que na 1ª posição da sequência temos 0
 valor = fibo(num-1)
 print(valor)
 rodape()

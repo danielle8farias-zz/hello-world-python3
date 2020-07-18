4de7c395b5cf193d723965b2f2be86c43f2ad230
Danielle <danielle8farias@gmail.com>
2020-07-17 21:16:48 -0300

    tratamento de excessões de dados de escolha do tema

--- exercicio_py/ex0111_jogo_forca/interface.py
+++ exercicio_py/ex0111_jogo_forca/interface.py
@@ -1,8 +1,14 @@
+import sys
+sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
+from numeros import ler_num_nat
+
+
+#criando menu e retornando opção escolhida
 def escolher_tema(lista_temas):
     c = 1
     for item in lista_temas:
         print(f'{c} - {item}')
         c += 1
     print()
-    opcao = int(input('Escolha um tema: '))
+    opcao = ler_num_nat('Escolha um tema: ')
     return opcao

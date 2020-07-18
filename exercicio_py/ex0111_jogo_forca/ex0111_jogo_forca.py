908e147984779cc10e09aeabd47199f5c150c3e0
Danielle <danielle8farias@gmail.com>
2020-07-17 21:15:56 -0300

    leitura dos dados dos temas

--- exercicio_py/ex0111_jogo_forca/ex0111_jogo_forca.py
+++ exercicio_py/ex0111_jogo_forca/ex0111_jogo_forca.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3.8
+
 import sys
 sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
 from mensagem import ler_cabecalho, rodape, ler_resposta
 
 from interface import escolher_tema
+from manipular_arquivos import abrir_temas
 
 from time import sleep
 
@@ -10,7 +13,7 @@ jogando = True
 
 while jogando:
     ler_cabecalho('jogo da forca')
-    temas = ['frutas', 'cores', 'planetas', 'capitais brasileiras', 'sair']
+    temas = abrir_temas()
     opcao = escolher_tema(temas)
 
     if opcao == 1:
@@ -32,3 +35,5 @@ while jogando:
         rodape()
         sleep(0.5)
         jogando = False
+    else:
+        print('Escolha uma opção válida!')

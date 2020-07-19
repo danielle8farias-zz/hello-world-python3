54c305fcb543c2eb78d30d03ba83c7f82faf6fb5
Danielle <danielle8farias@gmail.com>
2020-07-19 19:20:43 -0300

    retirando quebra de linha ao ler arquivo

--- exercicio_py/ex0111_jogo_forca/manipular_arquivos.py
+++ exercicio_py/ex0111_jogo_forca/manipular_arquivos.py
@@ -1,8 +1,11 @@
 def abrir_arquivo(nome):
     arq = open(nome, 'rt')
 
+    lista_itens = []
     for linha in arq:
+        #removendo quebra de linha dos arquivos
+        item = linha.replace('\n', '')
         #criando uma lista dos dados do arquivo
-        lista_itens = linha.split(', ')
+        lista_itens.append(item)
     
     return lista_itens

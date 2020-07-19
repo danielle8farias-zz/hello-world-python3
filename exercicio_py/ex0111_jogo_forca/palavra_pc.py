029f3ec9633d76b49c6571f96fab39a9246bbc89
Danielle <danielle8farias@gmail.com>
2020-07-19 18:04:44 -0300

    ocultando palavra escolhida do usuário

--- exercicio_py/ex0111_jogo_forca/palavra_pc.py
+++ exercicio_py/ex0111_jogo_forca/palavra_pc.py
@@ -10,7 +10,6 @@ from time import sleep
 
 def escolher_palavra(lista_palavras):
     palavra_escolhida = choice(lista_palavras)
-    print(palavra_escolhida)
     return palavra_escolhida, len(palavra_escolhida)
 
 

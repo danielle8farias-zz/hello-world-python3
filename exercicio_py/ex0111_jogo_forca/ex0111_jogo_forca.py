fc861ddc0f75b74e35a04f492f34fdb009f29245
Danielle <danielle8farias@gmail.com>
2020-07-19 20:19:09 -0300

    funcionando outros temas

--- exercicio_py/ex0111_jogo_forca/ex0111_jogo_forca.py
+++ exercicio_py/ex0111_jogo_forca/ex0111_jogo_forca.py
@@ -36,36 +36,36 @@ while jogando:
         print(f'\nO tema escolhido foi:')
         ler_cabecalho(f'{temas[opcao - 1]}')
         #abrindo arquivo com nome das cores
-        lista_frutas = abrir_arquivo('cores.txt')
+        lista_cores = abrir_arquivo('cores.txt')
         #recebendo retorno de palavra escolhida e tamanho
-        fruta_escolhida, tamanho = escolher_palavra(lista_frutas)
+        cor_escolhida, tamanho = escolher_palavra(lista_cores)
         #recebendo retorno de palavra secreta
         palavra_oculta = palavra_secreta(tamanho)
-        adivinhar_palavra(tamanho, palavra_oculta, fruta_escolhida)
+        adivinhar_palavra(tamanho, palavra_oculta, cor_escolhida)
     #planetas
     elif opcao == 3:
         sleep(0.5)
         print(f'\nO tema escolhido foi:')
         ler_cabecalho(f'{temas[opcao - 1]}')
-        #abrindo arquivo com nome das cores
-        lista_frutas = abrir_arquivo('planetas.txt')
+        #abrindo arquivo com nome dos planetas
+        lista_planetas = abrir_arquivo('planetas.txt')
         #recebendo retorno de palavra escolhida e tamanho
-        fruta_escolhida, tamanho = escolher_palavra(lista_frutas)
+        planeta_escolhido, tamanho = escolher_palavra(lista_planetas)
         #recebendo retorno de palavra secreta
         palavra_oculta = palavra_secreta(tamanho)
-        adivinhar_palavra(tamanho, palavra_oculta, fruta_escolhida)
+        adivinhar_palavra(tamanho, palavra_oculta, planeta_escolhido)
     #capitais brasileiras
     elif opcao == 4:
         sleep(0.5)
         print(f'\nO tema escolhido foi:')
         ler_cabecalho(f'{temas[opcao - 1]}')
-        #abrindo arquivo com nome das cores
-        lista_frutas = abrir_arquivo('capitais_br.txt')
+        #abrindo arquivo com nome das capitais brasileiras
+        lista_capitais = abrir_arquivo('capitais_br.txt')
         #recebendo retorno de palavra escolhida e tamanho
-        fruta_escolhida, tamanho = escolher_palavra(lista_frutas)
+        capital_escolhida, tamanho = escolher_palavra(lista_capitais)
         #recebendo retorno de palavra secreta
         palavra_oculta = palavra_secreta(tamanho)
-        adivinhar_palavra(tamanho, palavra_oculta, fruta_escolhida)
+        adivinhar_palavra(tamanho, palavra_oculta, capital_escolhida)
     elif opcao == 5:
         sleep(0.5)
         print('Finalizando o jogo')

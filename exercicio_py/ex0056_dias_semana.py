b955494f3a1f1511065b08ab953570f2189299b4
Danielle <danielle8farias@gmail.com>
2020-07-23 21:40:06 -0300

    descrevendo tupla sem os parênteses

--- exercicio_py/ex0056_dias_semana.py
+++ exercicio_py/ex0056_dias_semana.py
@@ -3,8 +3,8 @@ Dias da semana armazenadas numa tupla. Usuário escohe um número inteiro de 0 a
 um desses dias armazenados, de acordo com a posição na tupla.
 '''
 
-dia_semana = ('domingo', 'segunda', 'terça',
-'quarta', 'quinta', 'sexta', 'sábado')
+#tupla pode ser descrita sem os parênteses, porém a vírgula é obrigatória
+dia_semana = 'domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'
 
 while True:
     dia = int(input('Escolha um número de 0 a 6: '))

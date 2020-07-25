eb805f48a86bdf0b27e43f3d872627d1c7455320
Danielle <danielle8farias@gmail.com>
2020-07-25 19:38:21 -0300

    if aplicado ao dicionário

--- exercicio_py/ex0072_dicionario.py
+++ exercicio_py/ex0072_dicionario.py
@@ -7,8 +7,8 @@ estado = {}
 brasil = []
 
 for i in range(3):
-    estado['UF'] = input('Digite o nome do estado: ')
-    estado['sigla'] = input('Digite a sigla do estado: ')
+    estado['UF'] = input('Digite o nome do estado: ').capitalize()
+    estado['sigla'] = input('Digite a sigla do estado: ').upper()
     #fazendo a cópia do dicionário
     brasil.append(estado.copy())
 print()
@@ -42,3 +42,25 @@ for i in brasil:
         print(v, end=' ')
     print()
 print()
+
+#checando a chave
+if 'UF' in estado2:
+    print(f'O estado {estado2["UF"]} já foi adicionado.')
+
+if 'UF' in estado1.keys():
+    print(f'O estado {estado1["UF"]} já foi adicionado.')
+
+#checando o valor
+if 'Manaus' not in estado1.values():
+    estado1['capital'] = 'Manaus'
+
+
+#adicionado nova chave e valor ao dicionário estado2
+if 'capital' not in estado2:
+    estado2['capital'] = 'Belém'
+
+
+print(brasil)
+print('UF' in estado1)
+print('UF' in estado2.keys())
+print('Belém' in estado2.values())

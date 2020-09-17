4de8c1a65573d1b7e642c753d41180eebd094baf
Danielle <danielle8farias@gmail.com>
2020-09-17 16:48:19 -0300

    setdefault

---
+++ exercicio_py/ex0072_dicionario/ex0072_dicionario9.py
@@ -0,0 +1,15 @@
+estado = {'UF': 'Amazonas', 'sigla': 'AM'}
+
+
+#usando o método setdefault
+#   retorna o valor da chave se houver
+print(estado.setdefault('sigla'))
+print()
+
+#cria a chave se não houver, com o valor None se não for passado nada
+print(estado.setdefault('area'))
+print()
+
+print(estado.setdefault('capital', 'Manaus'))
+print(estado)
+print()

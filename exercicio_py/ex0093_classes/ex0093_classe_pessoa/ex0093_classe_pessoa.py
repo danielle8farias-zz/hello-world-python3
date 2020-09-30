ba3f1f111db4dea3adb727ce28004b78165fbd3f
Danielle <danielle8farias@gmail.com>
2020-09-30 17:13:06 -0300

    movendo arquivo

--- exercicio_py/ex0093_classes/ex0093_classe_pessoa/ex0093_classe_pessoa.py
+++ exercicio_py/ex0093_classes/ex0093_classe_pessoa/ex0093_classe_pessoa.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3.8
-
 #importando módulo de data
 from datetime import date
 
@@ -113,66 +111,3 @@ class Pessoa:
         idade = cls.ano_atual - ano_nasc
         return cls(nome, idade)
 
-
-#programa principal
-#instanciando objeto
-p1 = Humano()
-#mostra na tela o endereço de memória do objeto
-print(p1)
-p1.nome = 'Danielle'
-#retorna a string na tela
-print(p1.nome)
-#chamando o método da classe
-p1.falar()
-#função print vazia não retorna nada; pula uma linha
-print()
-
-p2 = Humano()
-print(p2)
-p2.nome = 'Rico'
-print(p2.nome)
-p2.falar()
-print()
-
-#método construtor sempre é chamado ao instanciar a classe
-# por isso aparece na tela o print de var
-p3 = Pessoa('Luis', 30)
-#retorna string formatada na tela
-print(f'Nome: {p3.nome}')
-#chamando método da classe
-p3.comer('maçã')
-#verificando se pode comer 2x
-p3.comer('melancia')
-p3.parar_comer()
-p3.comer('melancia')
-p3.falar('política')
-p3.parar_comer()
-p3.falar('política')
-p3.comer('tomate')
-print()
-
-#p4.nome = 'Sandra'
-#p4.idade = 50
-p4 = Pessoa('Sandra', 50)
-p4.outro_metodo()
-p4.parar_comer()
-p4.falar('culinária')
-p4.falar('cães')
-p4.parar_falar()
-p4.parar_falar()
-print()
-
-#imprimindo a variável da classe através da instância
-print(p3.ano_atual)
-print(p4.ano_atual)
-#imprimindo a variável da classe através da própria classe
-print(Pessoa.ano_atual)
-print()
-
-print(f'{p3.nome} nasceu em {p3.ano_nascimento()}.')
-print(f'{p4.nome} nasceu em {p4.ano_nascimento()}.')
-print()
-
-#instanciando objeto pelo método de classe
-p5 = Pessoa.por_ano_nasc('Enrico', 2000)
-print(f'{p5.nome} tem {p5.idade} anos.')

2841c209dff6d05dfbd134db846a818d67b5e87d
Danielle <danielle8farias@gmail.com>
2020-10-01 21:20:38 -0300

    falta organizar os joguinhos

---
+++ joguinhos/ex0111_jogo_forca/interface.py
@@ -0,0 +1,82 @@
+import sys
+sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
+from numeros import ler_num_nat
+
+
+#criando menu e retornando opção escolhida
+def escolher_tema(lista_temas):
+    c = 1
+    for item in lista_temas:
+        print(f'{c} - {item}')
+        c += 1
+    print()
+    opcao = ler_num_nat('Escolha um tema: ')
+    return opcao
+
+
+def desenhar_forca():
+    desenho = ['''
+    +----+
+    |    |
+    |
+    |
+    |
+    |
+    |
+    ===========''',
+    '''
+    +----+
+    |    |
+    |    O
+    |
+    |
+    |
+    |
+    ===========''',
+    '''
+    +----+
+    |    |
+    |    O
+    |    |
+    |
+    |
+    |
+    ===========''',
+    '''
+    +----+
+    |    |
+    |    O
+    |   /|
+    |
+    |
+    |
+    ===========''',
+    '''
+    +----+
+    |    |
+    |    O
+    |   /|\\
+    |
+    |
+    |
+    ===========''',
+    '''
+    +----+
+    |    |
+    |    O
+    |   /|\\
+    |   / 
+    |
+    |
+    ===========''',
+    '''
+    +----+
+    |    |
+    |    O
+    |   /|\\
+    |   / \\
+    |
+    |
+    ===========   
+    ''']
+    return desenho
---
+++ joguinhos/ex0111_jogo_forca/manipular_arquivos.py
@@ -0,0 +1,12 @@
+def abrir_arquivo(nome):
+    arq = open(nome, 'rt')
+
+    lista_itens = []
+    for linha in arq:
+        #removendo quebra de linha dos arquivos
+        item = linha.replace('\n', '')
+        #criando uma lista dos dados do arquivo
+        lista_itens.append(item)
+    
+    arq.close()
+    return lista_itens

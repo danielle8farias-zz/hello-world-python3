7eaf53f1aa0311c0dd8324f8d3d228df9d239dc5
Danielle <danielle8farias@gmail.com>
2020-10-01 21:20:00 -0300

    arquivos excluídos

--- exercicio_py/ex0093_classes/ex0093_classe_pessoa/ex0093_classe_pessoa3.py
+++
@@ -1,8 +0,0 @@
-#importando a classe que será usada para montar o objeto
-from ex0093_classe_pessoa import Pessoa
-
-print()
-#instanciando objeto pelo método de classe
-p5 = Pessoa.por_ano_nasc('Victor', 2000)
-print(f'{p5.nome} tem {p5.idade} anos.')
-print()

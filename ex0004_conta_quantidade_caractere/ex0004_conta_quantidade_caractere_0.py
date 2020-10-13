2ff022f1da8838dd62263ba2b0a30cbb2ce7489e
Danielle <danielle8farias@gmail.com>
2020-10-13 17:15:27 -0300

    contar quantidade de caracteres excluindo espaços

---
+++ ex0004_conta_quantidade_caractere/ex0004_conta_quantidade_caractere_0.py
@@ -0,0 +1,17 @@
+########
+# autora: danielle8farias@gmail.com 
+# repositório: https://github.com/danielle8farias
+# Descrição: Usuário informa nome e sobrenome. O programa retorna quantas letras o nome completo possui (excluindo espaços) e quantas letras o primeiro nome possui. 
+########
+
+nome_completo = input('Digite seu nome completo: ')
+
+#count(' ') conta os espaços em branco
+#len() retorna o tamanho da string
+#   retirando espaço entre os nomes
+tamanho_completo = len(nome_completo) - nome_completo.count(' ')
+print(f'Seu nome completo possui: {tamanho_completo} letras.')
+#find() retorna a posição de um caractere
+#   nesse caso queremos enccontrar o primeiro espaço
+num = nome_completo.find(' ')
+print(f'Seu primeiro nome possui: {num} letras.\n')

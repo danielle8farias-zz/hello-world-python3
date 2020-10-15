0f2651dc411bddd4e7e4c3657b2d6978650c66b8
Danielle <danielle8farias@gmail.com>
2020-10-15 19:49:36 -0300

    adicionando explicação detalhada sobre metade de n no cálculo do número primo

--- exercicio_py/ex0021_numero_primo/main_v1.py
+++ exercicio_py/ex0021_numero_primo/main_v1.py
@@ -12,9 +12,9 @@ from mensagem import *
 from numeros import ler_num_nat
 
 def verificar_primo(n):
-    raiz = int(n ** (0.5))
-    #raiz+1 para incluir a própria raiz; pois range tem intervalo aberto
-    for divisor in range(2, raiz+1):
+    metade = int(n//2)
+    #metade+1 para incluir a própria metade; pois range tem intervalo aberto
+    for divisor in range(2, metade+1):
         if n % divisor == 0:
             return False
     return True
@@ -35,12 +35,32 @@ while True:
     criar_linha()
 criar_rodape()
 
-####    explicação do uso da raiz   ####
-# n = 2 * a
-#    a = n / 2
-#    pois não existe divisor que seja maior do que a metade do número ou menor do que ele mesmo
-# n = a * b
-#    b = n / a
-# se a > raiz(n), então n/a < n/raiz(n)
-#    efetuando os cálculos de radiciação para retitrar a raiz do denominador, temos
-#    n/a < raiz(n), logo b < raiz(n)
+####    EXPLICAÇÃO DO USO DA METADE DE N   ####
+# O menor divisor diferente de 1 é o número 2.
+# Decompondo um número qualquer em dois fatores, sendo um deles o número 2, temos:
+# N = 2 * M
+# 'N' é o nosso número qualquer e 'M' a sua metade. Logo,
+# M = N / 2
+# Isso significa que, para divisões inteiras, o divisor não pode ser maior do que a metade do número e menor do que ele mesmo.
+#
+# Exemplo:
+# para N = 10, temos M = 5.
+# 10 = 2 * 5
+# 5 = 10 / 2
+# Então, o divisor de 10 não pode ser 6, 7, 8 ou 9.
+# Os divisores de 10 são: 1, 2, 5 e 10.
+# 
+# Outro exemplo:
+# para N = 45, temos M = 22,5.
+# 45 = 2 * 22,5
+# 22,5 = 45 / 2
+# Então, o divisor de 45 não pode ser 23, 24, ... ou 44.
+# Os divisores de 45 são: 1, 3, 5, 9, 15 e 45.
+#
+# então o range do laço for ao invés de ser:
+# range(2, n)
+# poderia ser:
+# range(2, n//2+1)
+# fazendo com que o final seja a metade, porém o valor inteiro + 1, porque o intervalo do range é aberto (isso é, vai até o valor - 1) e queremos incluir essa metade.
+# 
+##############

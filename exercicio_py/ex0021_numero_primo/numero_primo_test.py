bb00552ee4b86d9d266dca77bd5e3f6878b46767
Danielle <danielle8farias@gmail.com>
2020-10-15 19:48:37 -0300

    corrigiindo teste

--- exercicio_py/ex0021_numero_primo/numero_primo_test.py
+++ exercicio_py/ex0021_numero_primo/numero_primo_test.py
@@ -6,7 +6,6 @@
 
 def verificar_primo(n):
     raiz = int(n ** (0.5))
-    #raiz+1 para incluir a própria raiz; pois range tem intervalo aberto
     for divisor in range(2, raiz+1):
         if n % divisor == 0:
             return False
@@ -33,14 +32,3 @@ def test_primo5():
 
 def test_primo6():
     assert verificar_primo(800) == False
-
-
-####    explicação do uso da raiz   ####
-# n = 2 * a
-#    a = n / 2
-#    pois não existe divisor que seja maior do que a metade do número ou menor do que ele mesmo
-# n = a * b
-#    b = n / a
-# se a > raiz(n), então n/a < n/raiz(n)
-#    efetuando os cálculos de radiciação para retitrar a raiz do denominador, temos
-#    n/a < raiz(n), logo b < raiz(n)

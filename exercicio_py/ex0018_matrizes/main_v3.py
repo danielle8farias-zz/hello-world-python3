ee91708dc036ff421a8b8664776ae3119175c2a5
Danielle <danielle8farias@gmail.com>
2020-12-27 21:25:31 -0300

    chamada do módulo de construir matrizes

--- exercicio_py/ex0018_matrizes/main_v3.py
+++ exercicio_py/ex0018_matrizes/main_v3.py
@@ -8,6 +8,7 @@ import sys
 sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
 from mensagem import ler_cabecalho, criar_rodape, ler_resposta
 from numeros import ler_num_int, ler_num_float
+from construir_matriz import construir_matriz
 from time import sleep
 
 
@@ -15,17 +16,6 @@ def linha_coluna ():
     num_linhas = ler_num_int('Digite o número de linhas: ')
     num_colunas = ler_num_int('Digite o número de colunas: ')
     return num_linhas, num_colunas
-
-
-def construir_matriz (num_linhas, num_colunas):
-    matriz = []
-    for i in range(num_linhas):
-        linha = []
-        for j in range(num_colunas):
-            valor = ler_num_float(f'Digite o valor A{i}{j}: ')
-            linha.append(valor)
-        matriz.append(linha)
-    return matriz
 
 
 def somar_matrizes (A, B):
@@ -39,6 +29,7 @@ def somar_matrizes (A, B):
             linha.append(valor_soma)
         soma.append(linha)
     return soma
+
 
 while True:
     ler_cabecalho('soma duas matrizes')

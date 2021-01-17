5f8097f2882d0cafd7e6ebcc86c232a103190868
Danielle <danielle8farias@gmail.com>
2021-01-17 17:50:09 -0300

    previsão do tempo para 5 dias

--- exercicio_py/ex0038_previsao_tempo/main_v0.py
+++ exercicio_py/ex0038_previsao_tempo/main_v0.py
@@ -6,7 +6,7 @@
 
 import requests
 import json
-from pprint import pprint
+from time import sleep
 
 chave_api = 'u5eoJmS3s9PJ0X7RYCxrKWWaZm1WJhws'
 
@@ -56,7 +56,7 @@ def pegar_codigo_local(latitude, longitude):
             return None
 
 
-def pegar_tempo(codigo_local, nome_local):
+def pegar_tempo(codigo_local):
     url_tempo_api = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/' \
     + codigo_local +'?apikey=' + chave_api + '&language=pt-br&metric=true'
 
@@ -65,30 +65,37 @@ def pegar_tempo(codigo_local, nome_local):
         print('Não foi possível obter a previsão do tempo')
         return None
     else:
-        tempo = (json.loads(requisicao.text))
-        print(pprint(tempo))
-
-#        try:
-#        #aqui temos um lista e um dicionário dentro
-#            tempo = (json.loads(requisicao.text))
-#            info_clima_tempo = {}
-#            info_clima_tempo['clima'] = tempo[0]['WeatherText']
-#            info_clima_tempo['temperatura'] = tempo[0]['Temperature']['Metric']['Value']
-#            info_clima_tempo['nome_local'] = nome_local
-#            return info_clima_tempo
-#        except:
-#            return None
+        try:
+        #aqui temos um lista e um dicionário dentro
+            previsao = (json.loads(requisicao.text))
+            info_clima_tempo = []
+            for dia in previsao['DailyForecasts']:
+                clima_dia = {}
+                clima_dia['dia'] = dia['EpochDate']
+                clima_dia['tempo_dia'] = dia['Day']['IconPhrase']
+                clima_dia['tempo_noite'] = dia['Night']['IconPhrase']
+                clima_dia['max'] = dia['Temperature']['Maximum']['Value']
+                clima_dia['min'] = dia['Temperature']['Minimum']['Value']
+                #usando timestamp
+                info_clima_tempo.append(clima_dia)
+            return info_clima_tempo
+        except:
+            return None
 
 
 #main
 coordenadas_dic = pegar_coordenadas()
 try:
     codigo_dic = pegar_codigo_local(coordenadas_dic['latitude'], coordenadas_dic['longitude'])
-    clima = pegar_tempo(codigo_dic['codigo_local'], codigo_dic['nome_local'])
-#    print('Clima em: ' + clima['nome_local'])
-#    print('Temperatura: ' + str(clima['temperatura']) + '°C')
-#    print('Tempo: ' + clima['clima'])
+    print('Clima hoje e para os próximos dias: ')
+    previsao = pegar_tempo(codigo_dic['codigo_local'])
+    for dia in previsao:
+        sleep(0.5)
+        print(dia['dia'])
+        print('Tempo para o dia: ' + dia['tempo_dia'])
+        print('Tempo para a noite: ' + dia['tempo_noite'])
+        print('Temperatura máxima: ' + str(dia['max']) + '°C')
+        print('Temperatura mínima: ' + str(dia['min']) + '°C\n')
+        sleep(0.5)
 except:
-    print('Erro. Não foi possível obter o clima atual.')
-
-
+    print('Erro. Não foi possível obter a previsão do tempo.')

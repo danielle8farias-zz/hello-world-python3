e5de797a0ad386e59d639f94c370bdffc544194c
Danielle <danielle8farias@gmail.com>
2021-05-31 19:12:06 -0300

    refatorando o código

--- exercicio_py/ex0018_matrizes/main_v5.py
+++ exercicio_py/ex0018_matrizes/main_v5.py
@@ -7,39 +7,11 @@
 import sys
 sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
 from mensagem import ler_cabecalho, criar_rodape, ler_resposta
-
-from time import sleep
-
-#função que imprime matriz
-def imprimir_matriz(matriz):
-    #percorrendo as linhas
-    for i in range(len(matriz)):
-        sleep(0.5)
-        #percorrendo as colunas
-        for j in range(len(matriz[0])):
-            print(f'[{matriz[i][j]:^7}]', end='', flush=True)
-        print()
-
-
-def lendo_matriz(nome):
-    try:
-        a = open(nome, 'rt')
-    except:
-        print('ERRO! Não foi possível abrir o arquivo')
-    else:
-        matriz = []
-        linha = a.readline()
-        while linha != "":
-            elementos = linha.split()
-            for i in range(len(elementos)):
-                elementos[i] = float(elementos[i])
-            matriz.append(elementos)
-            linha = a.readline()
-        a.close()
-        imprimir_matriz(matriz)
+from funcoes_matriz import imprimir_matriz, ler_matriz
 
 
 ler_cabecalho('abrindo matriz de um arquivo')
 arquivo = 'matriz4x5.txt'
-lendo_matriz(arquivo)
+matriz = ler_matriz(arquivo)
+imprimir_matriz(matriz, 4, 5)
 criar_rodape()

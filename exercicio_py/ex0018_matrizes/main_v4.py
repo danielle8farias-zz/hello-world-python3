1de1ed0bfad51ca582ddd57311f1d1c7d21dc744
Danielle <danielle8farias@gmail.com>
2021-05-31 17:23:49 -0300

    refatoração do código

--- exercicio_py/ex0018_matrizes/main_v4.py
+++ exercicio_py/ex0018_matrizes/main_v4.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
 from mensagem import ler_cabecalho, criar_rodape, ler_resposta
 from numeros import ler_num_int, ler_num_float
-from construir_matriz import construir_matriz
+from funcoes_matriz import construir_matriz, imprimir_matriz, multiplicar_matrizes
 from time import sleep
 
 
@@ -16,29 +16,6 @@ def linha_coluna ():
     num_linhas = ler_num_int('Digite o número de linhas: ')
     num_colunas = ler_num_int('Digite o número de colunas: ')
     return num_linhas, num_colunas
-
-
-def imprimir_matriz(M, Mi, Mj):
-    for i in range(Mi):
-        for j in range(Mj):
-            sleep(0.5)
-            print(f'[{M[i][j]:^5}]', end='', flush=True)
-        print()
-
-
-def multiplicar_matrizes (A, B):
-    num_linhas_A, num_colunas_A = len(A), len(A[0])
-    num_linhas_B, num_colunas_B = len(B), len(B[0])
-    multiplicacao = []
-    for i in range(num_linhas_A):
-        linha = []
-        for j in range(num_colunas_B):
-            soma = 0
-            for k in range(num_colunas_A):
-                soma += A[i][k] * B[k][j]
-            linha.append(soma)
-        multiplicacao.append(linha)
-    return multiplicacao
 
 
 while True:

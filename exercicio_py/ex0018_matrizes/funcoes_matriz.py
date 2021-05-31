b4e27987e9824848ea990bc7cf994bbe8ffa472a
Danielle <danielle8farias@gmail.com>
2021-05-31 20:22:21 -0300

    inserindo a função de multiplicação de uma matriz por um número

--- exercicio_py/ex0018_matrizes/funcoes_matriz.py
+++ exercicio_py/ex0018_matrizes/funcoes_matriz.py
@@ -100,3 +100,34 @@ def ler_matriz(nome):
             linha = a.readline()
         a.close()
         return matriz
+
+
+def multiplicar_real(A, n):
+    '''
+    Recebe uma matriz e um número e retorna a multiplicação
+    dessa matriz por esse número.
+    '''
+    #len() pega o tamanho da lista (matriz) A
+    #   número de linhas é o comprimento da matriz
+    num_linhas = len(A)
+    #número de colunas é o tamanho de uma das sublistas de matriz
+    num_colunas = len(A[0])
+    #criando lista vazia que armazenará o resultado da multiplicação
+    multi_real = []
+    #laço for
+    #   para cada item até o número de linhas da matriz
+    #   range() vai de zero até (num_linhas -1)
+    #   percorrendo as linhas da matriz
+    for i in range(num_linhas):
+        #criando a lista para as linhas
+        linha = []
+        #percorrendo as colunas da matriz
+        for j in range(num_colunas):
+            #multiplicando cada item da matriz pelo número real
+            valor = (n * A[i][j])
+            #adiciona o resultado da multiplicação ao final da lista linha
+            linha.append(valor)
+        #adiciona cada uma das linhas a matriz que guardará o resultado
+        multi_real.append(linha)
+    #retorna a nova matriz com o resultado das multiplicações
+    return multi_real

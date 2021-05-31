dcb5ec947ceb1e48643e65bde3e813195863ac25
Danielle <danielle8farias@gmail.com>
2021-05-31 20:39:59 -0300

    refatorando o código

--- exercicio_py/ex0018_matrizes/main_v6.py
+++ exercicio_py/ex0018_matrizes/main_v6.py
@@ -4,40 +4,25 @@
 # Descrição: Multiplicação de uma matriz por um número.
 ########
 
-#   A é a matriz
-#   n é o número
-def multiplicar_real(A, n):
-    #len() pega o tamanho da lista (matriz) A
-    #   número de linhas é o comprimento da matriz
-    num_linhas = len(A)
-    #número de colunas é o tamanho de uma das sublistas de matriz
-    num_colunas = len(A[0])
-    #criando lista vazia que armazenará o resultado da multiplicação
-    multi_real = []
-    #laço for
-    #   para cada item até o número de linhas da matriz
-    #   range() vai de zero até (num_linhas -1)
-    #   percorrendo as linhas da matriz
-    for i in range(num_linhas):
-        #criando a lista para as linhas
-        linha = []
-        #percorrendo as colunas da matriz
-        for j in range(num_colunas):
-            #multiplicando cada item da matriz pelo número real
-            valor = (n * A[i][j])
-            #adiciona o resultado da multiplicação ao final da lista linha
-            linha.append(valor)
-        #adiciona cada uma das linhas a matriz que guardará o resultado
-        multi_real.append(linha)
-    #print() imprime da tela o retorno da função
-    print(multi_real)
-    #retorna a nova matriz com o resultado das multiplicações
-    return multi_real
+import sys
+sys.path.append('/home/danielle8farias/hello-world-python3/meus_modulos')
+from mensagem import ler_cabecalho, criar_rodape, criar_linha
+from numeros import ler_num_nat, ler_num_float
+from funcoes_matriz import construir_matriz, imprimir_matriz, multiplicar_real
 
 
-if __name__ == '__main__':
-    #chamada da função
-    #   matriz = [[1,2],[3,4]]
-    #   número real = 5
-    multiplicar_real([[1,2],[3,4]], 5) # [[5, 10], [15, 20]]
-    multiplicar_real([[1,2],[3,4]], 2.5) # [[2.5, 5.0], [7.5, 10.0]]
+ler_cabecalho('matriz x número')
+linha = ler_num_nat('Digite o número de linhas: ')
+coluna = ler_num_nat('Digite o número de colunas: ')
+print()
+matriz_A = construir_matriz(linha, coluna)
+print()
+imprimir_matriz(matriz_A, linha, coluna)
+num = ler_num_float('\nDigite um número para multiplicar: ')
+matriz_M = multiplicar_real(matriz_A, num)
+criar_linha()
+print('Nova matriz multiplicada:')
+criar_linha()
+imprimir_matriz(matriz_M, linha, coluna)
+print()
+criar_rodape()
